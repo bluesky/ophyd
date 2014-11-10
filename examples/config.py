1ea08be6d1b1a658099496b1dac2152643f4b125
Ken L <klauer@bnl.gov>
2014-11-10 12:15:24 -0500

    ENH: Examples directory

---
+++ examples/config.py
@@ -0,0 +1,40 @@
+'''
+'''
+
+import sys
+import logging
+
+try:
+    import ophyd
+except ImportError:
+    sys.path.insert(0, '..')
+    import ophyd
+
+
+LOG_FORMAT = "%(asctime)-15s [%(name)5s:%(levelname)s] %(message)s"
+EXAMPLE_LOGGER = 'ophyd_examples'
+
+
+def setup_loggers(logger_names, fmt=LOG_FORMAT):
+    fmt = logging.Formatter(LOG_FORMAT)
+    for name in logger_names:
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(fmt)
+        logger.addHandler(handler)
+
+
+session = ophyd.get_session_manager()
+
+setup_loggers((EXAMPLE_LOGGER, ))
+logger = logging.getLogger(EXAMPLE_LOGGER)
+
+motor_recs = ['XF:31IDA-OP{Tbl-Ax:X1}Mtr',
+              'XF:31IDA-OP{Tbl-Ax:X2}Mtr',
+              'XF:31IDA-OP{Tbl-Ax:X3}Mtr',
+              'XF:31IDA-OP{Tbl-Ax:X4}Mtr',
+              'XF:31IDA-OP{Tbl-Ax:X5}Mtr',
+              'XF:31IDA-OP{Tbl-Ax:X6}Mtr',
+              ]
---
+++ examples/motor.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2.7
+'''
+A simple test for :class:`EpicsMotor`
+'''
+
+import time
+
+import config
+from ophyd.controls import EpicsMotor
+
+
+def callback(sub_type=None, timestamp=None, value=None, **kwargs):
+    config.logger.info('[callback] [%s] (type=%s) value=%s' % (timestamp, sub_type, value))
+
+
+def done_moving(**kwargs):
+    config.logger.info('Done moving %s' % (kwargs, ))
+
+
+def test():
+    loggers = ('ophyd.controls.signal',
+               'ophyd.controls.positioner',
+               'ophyd.session',
+               )
+
+    config.setup_loggers(loggers)
+    logger = config.logger
+
+    motor_record = config.motor_recs[0]
+
+    m1 = EpicsMotor(motor_record)
+    # m2 = EpicsMotor('MLL:bad_record')
+    m1.subscribe(callback, event_type=m1.SUB_DONE)
+
+    m1.user_readback.subscribe(callback)
+    # print(m1.user_readback.read())
+    # print(m1.read())
+
+    logger.info('--> move to 1')
+    m1.move(1)
+    time.sleep(1)
+    logger.info('--> move to 0')
+    m1.move(0, moved_cb=done_moving)
+
+    # m2.move(1)
+    # time.sleep(1)
+
+
+if __name__ == '__main__':
+    test()
