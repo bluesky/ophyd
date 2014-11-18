a8d2cba7c42f702d44c92bcbd3c32fd0159e9b9a
Ken L <klauer@bnl.gov>
2014-11-18 10:46:06 -0500

    ENH: script to grab example IOCs (no submodules)

---
+++ examples/iocs/clone_iocs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Grab some EPICS IOCS for testing.
+
+# Motor record-based motor simulation
+git clone https://github.com/dchabot/motorsim
+
+# areaDetector simulator
+git clone https://github.com/dchabot/adsim
