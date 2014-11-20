15d9d4ae4210f97c5949c74233a0ba726892fca5
Daron Chabot <dchabot@bnl.gov>
2014-11-20 12:47:12 -0500

    ENH: add scaler class example.

--- examples/scaler.py
+++ examples/scaler.py
@@ -9,18 +9,46 @@ def enum(**enums):
 
 ScalerMode = enum(ONE_SHOT=0, AUTO_COUNT=1)
 
+def test():
+    loggers = ('ophyd.controls.signal',
+               'ophyd.controls.scaler',
+               'ophyd.session',
+               )
 
-sca = scaler.Scaler('XF:23ID2-ES{Sclr:1}')
-sca.start()
-print 'Sleeping...'
-time.sleep(2)
-print 'Done sleeping.'
-sca.stop()
-#print 'Set mode to AutoCount'
-#sca.set_mode(ScalerMode.AUTO_COUNT)
-#time.sleep(0.5)
-#print 'Set mode to OneShot'
-#sca.set_mode(ScalerMode.ONE_SHOT)
-print 'Trigger read() method...'
-vals = sca.read()
-print vals
+    config.setup_loggers(loggers)
+    logger = config.logger
+
+
+    sca = scaler.Scaler(config.scalers[0])
+
+    sca.preset_time = 5.2
+
+    logger.info('Counting in One-Shot mode for %f s...' % sca.preset_time)
+    sca.start()
+    logger.info('Sleeping...')
+    time.sleep(3)
+    logger.info('Done sleeping. Stopping counter...')
+    sca.stop()
+
+    logger.info('Set mode to AutoCount')
+    sca.count_mode = ScalerMode.AUTO_COUNT
+    sca.start()
+    logger.info('Begin auto-counting (aka "background counting")...')
+    time.sleep(2)
+    logger.info('Set mode to OneShot')
+    sca.count_mode = ScalerMode.ONE_SHOT
+    time.sleep(1)
+    logger.info('Stopping (aborting) auto-counting.')
+    sca.stop()
+
+    logger.info('read() all channels in one-shot mode...')
+    vals = sca.read()
+    logger.info(vals)
+
+    channels = (1,3,5,6)
+    logger.info('read() selected channels %s in one-shot mode...' % list(channels))
+    vals = sca.read(channels)
+    logger.info(vals)
+
+if __name__ == '__main__':
+    test()
