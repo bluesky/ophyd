88b8d393c87cb5526852b22d3fbf22fb0b536919
Ken L <klauer@bnl.gov>
2014-12-02 16:21:25 -0500

    MNT: remove exlog

--- ophyd/utils/exlog.py
+++
@@ -1,9 +0,0 @@
-# vi: ts=4 sw=4 sts=4 expandtab
-'''
-:mod:`ophyd.utils.exlog` - ExLog-related utilities
-==================================================
-
-.. module:: ophyd.utils.exlog
-   :synopsis:
-
-'''
