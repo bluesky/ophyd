0e9a7ba60814ae268c45f633559e89b8cc14d8fc
Daron Chabot <dchabot@bnl.gov>
2014-12-04 18:01:55 -0500

    MNT: decouple 23id example config from config.py.

--- examples/xf23id1_conf.py
+++ examples/xf23id1_conf.py
@@ -1,5 +1,3 @@
-import config
-
 import numpy as np
 
 from ophyd.controls import EpicsMotor, EpicsScaler, PVPositioner, EpicsSignal
@@ -9,21 +7,20 @@ from scan1d import scan1d
 #from examples.dumb_scan import simple_scan
 
 
-#slt1_i = EpicsMotor('XF:23ID1-OP{Slt:1-Ax:I}Mtr', name='slt1_i')
-#slt1_o = EpicsMotor('XF:23ID1-OP{Slt:1-Ax:O}Mtr', name='slt1_o')
-m1 = EpicsMotor(config.motor_recs[0], name='m1')
-m2 = EpicsMotor(config.motor_recs[1], name='m2')
-fm = config.fake_motors[0]
-m7 = PVPositioner(fm['setpoint'],
-                       readback=fm['readback'],
-                       act=fm['actuate'], act_val=1,
-                       stop=fm['stop'], stop_val=1,
-                       done=fm['moving'], done_val=1,
-                       put_complete=False,
-                       name='m7'
-                       )
-sensor1 = EpicsSignal(config.fake_sensors[0], rw=False, name='sensor1')
-sensor2 = EpicsSignal(config.fake_sensors[1], rw=False, name='sensor2')
+slt1_i = EpicsMotor('XF:23ID1-OP{Slt:1-Ax:I}Mtr', name='slt1_i')
+slt1_o = EpicsMotor('XF:23ID1-OP{Slt:1-Ax:O}Mtr', name='slt1_o')
+m1 = EpicsMotor('XF:31IDA-OP{Tbl-Ax:X1}Mtr', name='m1')
+m2 = EpicsMotor('XF:31IDA-OP{Tbl-Ax:X2}Mtr', name='m2')
+m7 = PVPositioner('XF:31IDA-OP{Tbl-Ax:FakeMtr}-SP',
+                   readback='XF:31IDA-OP{Tbl-Ax:FakeMtr}-I',
+                   act='XF:31IDA-OP{Tbl-Ax:FakeMtr}Cmd:Go-Cmd', act_val=1,
+                   stop='XF:31IDA-OP{Tbl-Ax:FakeMtr}Cmd:Stop-Cmd', stop_val=1,
+                   done='XF:31IDA-OP{Tbl-Ax:FakeMtr}Sts:Moving-Sts', done_val=1,
+                   put_complete=False,
+                   name='m7'
+                   )
+sensor1 = EpicsSignal('XF:31IDA-BI{Dev:1}E-I', rw=False, name='sensor1')
+sensor2 = EpicsSignal('XF:31IDA-BI{Dev:2}E-I', rw=False, name='sensor2')
 sclr_trig = EpicsSignal('XF:23ID2-ES{Sclr:1}.CNT', rw=True, name='sclr_trig')
 sclr_ch1 = EpicsSignal('XF:23ID2-ES{Sclr:1}.S1', rw=False, name='sclr_ch1')
 sca = EpicsScaler('XF:23ID2-ES{Sclr:1}', name='sca')
@@ -40,7 +37,7 @@ args = ('XF:23ID1-OP{Mono}Enrgy-SP',
         'done_val': 0,
         'name': 'pgm_energy'
         })
-pgm_energy = PVPositioner(args[0], **args[1])
+#pgm_energy = PVPositioner(args[0], **args[1])
 
 # initialize M1A virtual axes Positioner
 args = ('XF:23IDA-OP:1{Mir:1-Ax:Z}Mtr_POS_SP',
@@ -53,10 +50,10 @@ args = ('XF:23IDA-OP:1{Mir:1-Ax:Z}Mtr_POS_SP',
          'done_val': 0,
          'name': 'm1a_z',
         })
-m1a_z = PVPositioner(args[0], **args[1])
+#m1a_z = PVPositioner(args[0], **args[1])
 
 # AreaDetector crud
-simdet = SimDetector(config.sim_areadetector[0]['prefix'])
+simdet = SimDetector('XF:31IDA-BI{Cam:Tbl}')
 # For now, access as simple 'signals'
 simdet_acq = EpicsSignal('XF:31IDA-BI{Cam:Tbl}cam1:Acquire_RBV',
                          write_pv='XF:31IDA-BI{Cam:Tbl}cam1:Acquire',
