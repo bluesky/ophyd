b4738d4c58accf848b66b71b11bb2efb87ceadb3
Stuart Wilkins <swilkins@bnl.gov>
2014-12-07 22:27:38 -0500

    Fixed (reverted example script)

--- examples/scan1d.py
+++ examples/scan1d.py
@@ -2,10 +2,10 @@ from ophyd.runengine import RunEngine
 
 run_eng = RunEngine(None)
 
-''' Assumes detector params (dwell time, etc) have been
+''' Assumes detector params (dwell time, etc) have been 
     previously configured.
 
-    Also assumes that ophyd.runengine.RunEngine exists in the
+    Also assumes that ophyd.runengine.RunEngine exists in the 
     IPython namespace.
 
     Returns a dict of {'data_key': [value1,value2,...],}
@@ -20,7 +20,7 @@ def scan1d(run_id, detectors=[], triggers=[], motors=[], paths=[], settle_time=N
     for path,motor in zip(paths,motors):
         motor.set_trajectory(path)
     scan_args['positioners'] = motors
-
+    
     scan_args['settle_time'] = settle_time
 
     return run_eng.start_run(run_id, scan_args=scan_args)
