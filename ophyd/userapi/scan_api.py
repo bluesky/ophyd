6f5f665fb379d258398c002aaa293c43b173cd6b
Stuart Wilkins <swilkins@bnl.gov>
2015-01-04 21:58:31 -0500

    Cleanup of api and added functionality to scan classes

--- ophyd/userapi/scan_api.py
+++ ophyd/userapi/scan_api.py
@@ -2,14 +2,19 @@ from __future__ import print_function
 import numpy as np
 from time import sleep, strftime
 import six
+
 from ..runengine import RunEngine
-from ..controls import EpicsMotor, PVPositioner
 from ..session import get_session_manager
 
 session_manager = get_session_manager()
-logger = get_session_manager()._logger
+logger = session_manager._logger
 
 __all__ = ['AScan', 'DScan']
+
+try:
+    logbook = session_manager['olog_client']
+except KeyError:
+    logbook = None
 
 
 def ensure_iterator(i):
@@ -20,16 +25,80 @@ def ensure_iterator(i):
         return [i]
 
 
-class Scan(object):
+def objects_to_str(iterables, iter_sep=', ', iter_start='(',
+                   iter_stop=')'):
+
+    text = '['
+    sep = ''
+
+    # To see if we need start/stop chars then we see if we
+    # have a length 1 iterator
+    l = 0
+    try:
+        l = len(iterables)
+    except TypeError:
+        pass
+
+    if l == 1:
+        # For a length 1 iterator we dont set the start
+        text = ''
+    else:
+        text = iter_start
+
+    for i in iterables:
+        # Is the object i iterable:
+        try:
+            iter(i)
+        except TypeError:
+            try:
+                # Try to see if we have a name attribute
+                text += '{}{}'.format(sep, i.name)
+            except AttributeError:
+                # Object does not have a name
+                pass
+            else:
+                # We have a valid name, break the iteration
+                break
+
+            text += '{}{}'.format(sep, str(i))
+
+        else:
+            text += '{}{}'.format(sep, objects_to_str(i))
+
+        # Now we have gone round once, set the separator
+        sep = iter_sep
+
+    if l != 1:
+        text += iter_stop
+
+    return text
+
+
+def cmdline_to_str(*args, **kwargs):
+    """Format a string based on the command line"""
+    msg = objects_to_str(args)
+    return msg
+
+
+class SharedState(object):
+    _shared_state = {}
 
     def __init__(self):
+        self.__dict__ = self._shared_state
+
+
+class Scan(SharedState):
+    def __init__(self, *args, **kwargs):
+        super(Scan, self).__init__(*args, **kwargs)
+
+        self._default_detectors = []
+        self._default_triggers = []
         self._run_eng = RunEngine(None)
+
         self._last_data = None
 
         self.triggers = None
         self.detectors = None
-        self._default_detectors = list()
-        self._default_triggers = list()
         self.settle_time = None
 
         self.paths = list()
@@ -39,7 +108,13 @@ class Scan(object):
         self._ploty = None
 
     def check_paths(self):
-        pass
+        """Check the paths against any limits of positioners"""
+        for pos, path in zip(self.positioners, self.paths):
+            for p in path:
+                if pos.check_value(p):
+                    raise ValueError('Scan moves positioner {} \
+                                     out of limits {},{}'.format(
+                                     pos.name, p.low_limit, p.high_limit))
 
     def __enter__(self):
         self.pre_scan()
@@ -62,8 +137,7 @@ class Scan(object):
 
     def format_plot(self):
         '''
-        Format the bits of information the user (most likely)
-        cares about
+        Guess the positioners and detectors that the user cares about
 
         Returns
         -------
@@ -89,13 +163,17 @@ class Scan(object):
         ploty = []
         # checking validity of self._ploty
         for name in valid_names:
-            if name in self._ploty:
-                ploty.append(name)
+            if self._ploty is not None:
+                if name in self._ploty:
+                    ploty.append(name)
         return plotx, ploty
 
-    def run(self):
+    def run(self, **kwargs):
         """Run the scan"""
         self.scan_id = session_manager.get_next_scan_id()
+
+        # Run this in a context manager to capture
+        # the KeyboardInterrupt
 
         with self:
             self.check_paths()
@@ -104,6 +182,8 @@ class Scan(object):
 
             for pos, path in zip(self.positioners, self.paths):
                 pos.set_trajectory(path)
+
+            # Create the dict to pass to the run-engine
 
             scan_args = dict()
             scan_args['detectors'] = self.detectors
@@ -115,6 +195,7 @@ class Scan(object):
             scan_args['custom']['plotx'] = plotx
             if ploty:
                 scan_args['custom']['ploty'] = ploty
+
             # Run the scan!
             self.data = self._run_eng.start_run(self.scan_id,
                                                 scan_args=scan_args)
@@ -155,46 +236,6 @@ class Scan(object):
         self._detectors = detectors
 
     @property
-    def default_detectors(self):
-        """Get the defualt detectors"""
-        return self._default_detectors
-
-    @default_detectors.setter
-    def default_detectors(self, det):
-        """Set the default detectors"""
-        self._default_detectors = det
-
-    @property
-    def default_triggers(self):
-        """Get the defualt triggers"""
-        return self._default_triggers
-
-    @default_triggers.setter
-    def default_triggers(self, trig):
-        """Set the default triggers"""
-        self._default_triggers = trig
-
-    @property
-    def positioners(self):
-        """Get the posiitoners for the scan"""
-        return self._positioners
-
-    @positioners.setter
-    def positioners(self, positioners):
-        """Set the positioners for the scan"""
-        self._positioners = positioners
-
-    @property
-    def paths(self):
-        """Get the paths for the scan"""
-        return self._paths
-
-    @paths.setter
-    def paths(self, paths):
-        """Set the paths for the scan"""
-        self._paths = paths
-
-    @property
     def ploty(self):
         return self._ploty
 
@@ -212,12 +253,16 @@ class Scan(object):
 
 
 class ScanND(Scan):
-
     """Class for a N-Dimensional Scan"""
 
-    def __init__(self):
-        Scan.__init__(self)
+    def __init__(self, *args, **kwargs):
+        super(ScanND, self).__init__()
         self.dimension = None
+        self.scan_command = None
+
+    def pre_scan(self, *args, **kwargs):
+        super(ScanND, self).pre_scan(*args, **kwargs)
+        self.make_log_entry()
 
     def calc_linear_path(self, start, stop, npts):
         """Return a linearaly spaced path"""
@@ -233,7 +278,53 @@ class ScanND(Scan):
         a = np.tile(a, y.prod())
         return a
 
-    def __call__(self, positioners, start, stop, npts):
+    def make_log_entry(self):
+        """Format and make a log entry for the scan"""
+        time_text = strftime("%a, %d %b %Y %H:%M:%S %Z")
+
+        # Print header
+
+        msg = 'Scan Command    : {}\n'.format(self.scan_command)
+        msg += 'Scan ID         : {}\n'.format(self.scan_id)
+        msg += 'Scan started at : {}\n'.format(time_text)
+        msg += 'Scan Dimension  : {}\n'.format(self.dimension)
+        msg += 'Scan Datapoints : {} ({})\n'.format(self.datapoints,
+                                                    np.prod(self.datapoints))
+
+        # Print positioners and start and stop values
+
+        msg += '\n{:<30} {:<20} {:<20}:\n'.format('Positioners',
+                                                  'Min', 'Max')
+        for pos, path in zip(self.positioners, self.paths):
+            msg += '{:<30} {:<20.8f} {:<20.8f}\n'.format(pos.name,
+                                                         min(path),
+                                                         max(path))
+
+        msg += '\nTriggers:\n'
+        for trig in self.triggers:
+            msg += '{:<30}\n'.format(trig.name)
+
+        msg += '\nDetectors:\n'
+        for det in self.detectors:
+            msg += '{:<30}\n'.format(det.name)
+
+        msg += '\n{0:=^80}\n'.format('')
+
+        for p in self.positioners + self.triggers + self.detectors:
+            try:
+                msg += 'PV:{}\n'.format(p.report['pv'])
+            except KeyError:
+                pass
+
+        logbook.log(msg)
+
+    def format_command_line(self, *args, **kwargs):
+        """Return a string representation of the passed arguments"""
+        cl_name = '{}()'.format(self.__class__.__name__)
+        cl_args = cmdline_to_str(*args, **kwargs)
+        return cl_name + cl_args
+
+    def __call__(self, positioners, start, stop, npts, **kwargs):
         """Run Scan"""
 
         # This is an n-dimensional scan. We take the dims from
@@ -248,10 +339,15 @@ class ScanND(Scan):
         pos = list()
         paths = list()
 
+        # Calculate number of points from intervals
+
         npts = np.asarray(npts) + 1
 
         start = ensure_iterator(start)
         stop = ensure_iterator(stop)
+
+        args = (positioners, start, stop, npts)
+        self.scan_command = self.format_command_line(*args, **kwargs)
 
         for b, e, d in zip(start, stop, range(dimension)):
             # For each dimension we work out the paths
@@ -263,49 +359,30 @@ class ScanND(Scan):
 
         self.positioners = pos
         self.paths = paths
+        self.datapoints = npts
+        self.dimension = dimension
 
-        self.run()
+        self.run(**kwargs)
 
 
 class AScan(ScanND):
-
-    def pre_scan(self):
-        pass
-        ScanND.pre_scan(self)
-
-        time_text = strftime("%a, %d %b %Y %H:%M:%S %Z")
-
-        msg = ''
-        msg += 'Scan ID         : {}\n'.format(self.scan_id)
-        msg += 'Scan started at : {}\n\n'.format(time_text)
-        msg += '===\n'
-        for p in self.positioners + self.triggers + self.detectors:
-            try:
-                msg += 'PV:{}\n'.format(p.report['pv'])
-            except KeyError:
-                pass
-
-        logger.info(msg)
+    pass
 
 
 class DScan(AScan):
-
-    def __init__(self):
-        AScan.__init__(self)
-
     def pre_scan(self):
-        """Prescan Compute Paths"""
+        """Prescan store starting positions and change paths"""
+        super(DScan, self).pre_scan()
         self._start_positions = [p.position for p in self.positioners]
         self.paths = [np.array(path) + start
                       for path, start in zip(self.paths, self._start_positions)]
-        AScan.pre_scan(self)
 
     def post_scan(self):
         """Post Scan Move to start positions"""
-        AScan.post_scan(self)
-
+        super(DScan, self).pre_scan()
         status = [pos.move(start, wait=False)
-                  for pos, start in zip(self.positioners, self._start_positions)]
+                  for pos, start in
+                  zip(self.positioners, self._start_positions)]
 
         print("\nMoving positioners back to start positions.")
         while any(not stat.done for stat in status):
