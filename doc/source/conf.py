f5bdda07d923cd21bfb86413c798c9fbc82e705c
Stuart Wilkins <swilkins@bnl.gov>
2015-01-11 12:43:12 -0500

    MNT: Added napoleon for google/numpy style docstrings

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ import sys, os
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
               'sphinx.ext.coverage', 'sphinx.ext.mathjax',
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode', 'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
