0ddae8f30d2279bfff3238672adb62c1e5814ba0
Stuart Wilkins <swilkins@bnl.gov>
2015-01-10 20:27:10 -0500

    Intermediate commit

--- ophyd/userapi/scan_api.py
+++ ophyd/userapi/scan_api.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 import numpy as np
 from time import sleep, strftime
 import six
+import sys
+
+from IPython.utils.coloransi import TermColors as tc
 
 from ..runengine import RunEngine
 from ..session import get_session_manager
@@ -9,7 +12,7 @@ from ..session import get_session_manager
 session_manager = get_session_manager()
 logger = session_manager._logger
 
-__all__ = ['AScan', 'DScan']
+__all__ = ['AScan', 'DScan', 'Scan']
 
 try:
     logbook = session_manager['olog_client']
@@ -17,82 +20,13 @@ except KeyError:
     logbook = None
 
 
-def ensure_iterator(i):
-    """Check if i is an iterator. If not return length-1 iterator"""
-    if hasattr(i, "__iter__") or hasattr(i, "__getitem__"):
-        return i
-    else:
-        return [i]
+class Scan(object):
+    _shared_config = {'default_triggers': [],
+                      'default_detectors': []}
 
-
-def objects_to_str(iterables, iter_sep=', ', iter_start='(',
-                   iter_stop=')'):
-
-    text = '['
-    sep = ''
-
-    # To see if we need start/stop chars then we see if we
-    # have a length 1 iterator
-    l = 0
-    try:
-        l = len(iterables)
-    except TypeError:
-        pass
-
-    if l == 1:
-        # For a length 1 iterator we dont set the start
-        text = ''
-    else:
-        text = iter_start
-
-    for i in iterables:
-        # Is the object i iterable:
-        try:
-            iter(i)
-        except TypeError:
-            try:
-                # Try to see if we have a name attribute
-                text += '{}{}'.format(sep, i.name)
-            except AttributeError:
-                # Object does not have a name
-                pass
-            else:
-                # We have a valid name, break the iteration
-                break
-
-            text += '{}{}'.format(sep, str(i))
-
-        else:
-            text += '{}{}'.format(sep, objects_to_str(i))
-
-        # Now we have gone round once, set the separator
-        sep = iter_sep
-
-    if l != 1:
-        text += iter_stop
-
-    return text
-
-
-def cmdline_to_str(*args, **kwargs):
-    """Format a string based on the command line"""
-    msg = objects_to_str(args)
-    return msg
-
-
-class SharedState(object):
-    _shared_state = {}
-
-    def __init__(self):
-        self.__dict__ = self._shared_state
-
-
-class Scan(SharedState):
     def __init__(self, *args, **kwargs):
         super(Scan, self).__init__(*args, **kwargs)
 
-        self._default_detectors = []
-        self._default_triggers = []
         self._run_eng = RunEngine(None)
 
         self._last_data = None
@@ -207,13 +141,34 @@ class Scan(SharedState):
 
     @data.setter
     def data(self, data):
+        """Set the last scanned data set"""
         self._last_data = data
+
+    @property
+    def default_detectors(self):
+        """Return the default detectors"""
+        return self._shared_config['default_detectors']
+
+    @default_detectors.setter
+    def default_detectors(self, detectors):
+        """Set the default detectors"""
+        self._shared_config['default_detectors'] = detectors
+
+    @property
+    def default_triggers(self):
+        """Return the default triggers"""
+        return self._shared_config['default_triggers']
+
+    @default_triggers.setter
+    def default_triggers(self, triggers):
+        """Set the default triggers"""
+        self._shared_config['default_triggers'] = triggers
 
     @property
     def triggers(self):
         """Return the default triggers for this scan"""
         if self._triggers is None:
-            return self._default_triggers
+            return self.default_triggers
         else:
             return self._triggers + self.default_triggers
 
@@ -226,30 +181,14 @@ class Scan(SharedState):
     def detectors(self):
         """Return the default detectors for this scan"""
         if self._detectors is None:
-            return self._default_detectors
+            return self.default_detectors
         else:
-            return self._detectors + self._default_detectors
+            return self._detectors + self.default_detectors
 
     @detectors.setter
     def detectors(self, detectors):
         """Set the default detectors for this scan"""
         self._detectors = detectors
-
-    @property
-    def ploty(self):
-        return self._ploty
-
-    @ploty.setter
-    def ploty(self, ploty):
-        '''
-
-        Parameters
-        ----------
-        ploty : str
-            The name of the data set that is understood to be the
-            important y data for the scan
-        '''
-        self._ploty = ploty
 
 
 class ScanND(Scan):
@@ -284,45 +223,58 @@ class ScanND(Scan):
 
         # Print header
 
-        msg = 'Scan Command    : {}\n'.format(self.scan_command)
-        msg += 'Scan ID         : {}\n'.format(self.scan_id)
-        msg += 'Scan started at : {}\n'.format(time_text)
-        msg += 'Scan Dimension  : {}\n'.format(self.dimension)
-        msg += 'Scan Datapoints : {} ({})\n'.format(self.datapoints,
-                                                    np.prod(self.datapoints))
+        msg = ['Scan Command    : {}'.format(self.scan_command)]
+        msg.append('Scan ID         : {}'.format(self.scan_id))
+        msg.append('Scan started at : {}'.format(time_text))
+        msg.append('Scan Dimension  : {}'.format(self.dimension))
+        msg.append('Scan Datapoints : {} ({})'.format(self.datapoints,
+                                                      np.prod(self.datapoints)))
 
         # Print positioners and start and stop values
 
-        msg += '\n{:<30} {:<20} {:<20}:\n'.format('Positioners',
-                                                  'Min', 'Max')
+        msg.append('')
+        msg.append('{:<30} {:<20} {:<20}:'.format('Positioners',
+                                                  'Min', 'Max'))
+
         for pos, path in zip(self.positioners, self.paths):
-            msg += '{:<30} {:<20.8f} {:<20.8f}\n'.format(pos.name,
-                                                         min(path),
-                                                         max(path))
-
-        msg += '\nTriggers:\n'
+            msg.append('{:<30} {:<20.8f} {:<20.8f}'.format(pos.name,
+                                                           min(path),
+                                                           max(path)))
+        msg.append('')
+        msg.append('Triggers:')
         for trig in self.triggers:
-            msg += '{:<30}\n'.format(trig.name)
+            msg.append('{:<30}'.format(trig.name))
 
-        msg += '\nDetectors:\n'
+        msg.append('')
+        msg.append('Detectors:')
         for det in self.detectors:
-            msg += '{:<30}\n'.format(det.name)
+            msg.append('{:<30}'.format(det.name))
 
-        msg += '\n{0:=^80}\n'.format('')
+        msg.append('')
+        msg.append('{0:=^80}'.format(''))
 
         for p in self.positioners + self.triggers + self.detectors:
             try:
-                msg += 'PV:{}\n'.format(p.report['pv'])
+                msg.append('PV:{}'.format(p.report['pv']))
             except KeyError:
                 pass
 
-        logbook.log(msg)
+        d = {}
+        d['id'] = self.scan_id
+        d['command'] = self.scan_command
+        d['triggers'] = repr(self.triggers)
+        d['detectors'] = repr(self.detectors)
+        d['positioners'] = repr(self.positioners)
+        d['start'] = repr(self.start)
+        d['stop'] = repr(self.stop)
+        d['npts'] = repr(self.npts)
+        logbook.log('\n'.join(msg), properties=[['OphydScan', d]])
 
     def format_command_line(self, *args, **kwargs):
         """Return a string representation of the passed arguments"""
-        cl_name = '{}()'.format(self.__class__.__name__)
         cl_args = cmdline_to_str(*args, **kwargs)
-        return cl_name + cl_args
+        rtn = '{}()({})'.format(self.__class__.__name__, cl_args)
+        return rtn
 
     def __call__(self, positioners, start, stop, npts, **kwargs):
         """Run Scan"""
@@ -330,24 +282,25 @@ class ScanND(Scan):
         # This is an n-dimensional scan. We take the dims from
         # the length of npts.
 
-        npts = ensure_iterator(npts)
         positioners = ensure_iterator(positioners)
         start = ensure_iterator(start)
         stop = ensure_iterator(stop)
+        npts = ensure_iterator(npts)
+
+        self.start = start
+        self.stop = stop
+        self.npts = npts
+
+        args = (positioners, start, stop, npts)
+        self.scan_command = self.format_command_line(*args, **kwargs)
 
         dimension = len(npts)
-        pos = list()
-        paths = list()
+        pos = []
+        paths = []
 
         # Calculate number of points from intervals
 
         npts = np.asarray(npts) + 1
-
-        start = ensure_iterator(start)
-        stop = ensure_iterator(stop)
-
-        args = (positioners, start, stop, npts)
-        self.scan_command = self.format_command_line(*args, **kwargs)
 
         for b, e, d in zip(start, stop, range(dimension)):
             # For each dimension we work out the paths
@@ -384,8 +337,71 @@ class DScan(AScan):
                   for pos, start in
                   zip(self.positioners, self._start_positions)]
 
-        print("\nMoving positioners back to start positions.")
+        print("\n")
+        print(tc.Red + "Moving positioners back to start positions.......",
+              end='')
+        sys.stdout.flush()
         while any(not stat.done for stat in status):
             sleep(0.01)
 
-        print("Done.")
+        print(tc.Green + " Done.")
+
+
+def ensure_iterator(i):
+    """Check if i is an iterator. If not return length-1 iterator"""
+    if hasattr(i, "__iter__") or hasattr(i, "__getitem__"):
+        return i
+    else:
+        return [i]
+
+
+def objects_to_str(iterables, iter_sep=', ', iter_start='(',
+                   iter_stop=')'):
+
+    text = []
+
+    # To see if we need start/stop chars then we see if we
+    # have a length 1 iterator
+
+    l = 0
+    try:
+        l = len(iterables)
+    except TypeError:
+        pass
+
+    for i in iterables:
+        # Is the object i iterable:
+        if isinstance(i, tuple):
+            text.append(objects_to_str(i))
+        elif isinstance(i, list):
+            text.append(objects_to_str(i, iter_start='[', iter_stop=']'))
+        else:
+            try:
+                # Try to see if we have a name attribute
+                text.append(i.name)
+            except AttributeError:
+                # Object does not have a name
+                pass
+            else:
+                # We have a valid name, break the iteration
+                break
+
+            text.append(repr(i))
+
+    text = iter_sep.join(text)
+
+    if l != 1:
+        return ''.join([iter_start, text, iter_stop])
+
+    return text
+
+
+def cmdline_to_str(*args, **kwargs):
+    """Format a string based on the command line"""
+    msg = [objects_to_str(args, iter_start='', iter_stop='')]
+    if len(kwargs) > 0:
+        msg.append(', ')
+        for key, value in kwargs.iteritems():
+            msg.append('{}={}'.format(key, objects_to_str(value)))
+
+    return ''.join(msg)
