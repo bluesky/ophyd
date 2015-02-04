51fbf50f7f3a07b6b836474415c335223872058b
Thomas A Caswell <tcaswell@gmail.com>
2015-02-04 17:53:38 -0500

    MNT : py3k compatibility changes

--- ophyd/userapi/__init__.py
+++ ophyd/userapi/__init__.py
@@ -1,4 +1,5 @@
-from cli_api import (mov, movr, set_pos, wh_pos, set_lm, log_pos,
+from __future__ import absolute_import
+from .cli_api import (mov, movr, set_pos, wh_pos, set_lm, log_pos,
                      log_pos_diff, log_pos_mov)
 
-from scan_api import (Scan, Count, AScan, DScan)
+from .scan_api import (Scan, Count, AScan, DScan)
