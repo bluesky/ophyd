BUG: fixes #58, scaler data are persisted with wrong shape.
The _get_info( ) function is broken and needs revisiting.

examples/bl_test_conf.py
@@ -7,8 +7,8 @@ from scan1d import scan1d
 #from examples.dumb_scan import simple_scan
 
 
-slt1_i = EpicsMotor('XF:23ID1-OP{Slt:1-Ax:I}Mtr', name='slt1_i')
-slt1_o = EpicsMotor('XF:23ID1-OP{Slt:1-Ax:O}Mtr', name='slt1_o')
+#slt1_i = EpicsMotor('XF:23ID1-OP{Slt:1-Ax:I}Mtr', name='slt1_i')
+#slt1_o = EpicsMotor('XF:23ID1-OP{Slt:1-Ax:O}Mtr', name='slt1_o')
 m1 = EpicsMotor('XF:31IDA-OP{Tbl-Ax:X1}Mtr', name='m1')
 m2 = EpicsMotor('XF:31IDA-OP{Tbl-Ax:X2}Mtr', name='m2')
 m7 = PVPositioner('XF:31IDA-OP{Tbl-Ax:FakeMtr}-SP',
@@ -29,27 +29,27 @@ m1.set_trajectory(np.linspace(-1,2,10))
 m2.set_trajectory(np.linspace(-1,2,10))
 
 # initialize Positioner for Mono Energy
-args = ('XF:23ID1-OP{Mono}Enrgy-SP',
-        {'readback': 'XF:23ID1-OP{Mono}Enrgy-I',
-        'stop': 'XF:23ID1-OP{Mono}Cmd:Stop-Cmd',
-        'stop_val': 1,
-        'done': 'XF:23ID1-OP{Mono}Sts:Move-Sts',
-        'done_val': 0,
-        'name': 'pgm_energy'
-        })
+#args = ('XF:23ID1-OP{Mono}Enrgy-SP',
+#        {'readback': 'XF:23ID1-OP{Mono}Enrgy-I',
+#        'stop': 'XF:23ID1-OP{Mono}Cmd:Stop-Cmd',
+#        'stop_val': 1,
+#        'done': 'XF:23ID1-OP{Mono}Sts:Move-Sts',
+#        'done_val': 0,
+#        'name': 'pgm_energy'
+#        })
 #pgm_energy = PVPositioner(args[0], **args[1])
 
 # initialize M1A virtual axes Positioner
-args = ('XF:23IDA-OP:1{Mir:1-Ax:Z}Mtr_POS_SP',
-        {'readback': 'XF:23IDA-OP:1{Mir:1-Ax:Z}Mtr_MON',
-         'act': 'XF:23IDA-OP:1{Mir:1}MOVE_CMD.PROC',
-         'act_val': 1,
-         'stop': 'XF:23IDA-OP:1{Mir:1}STOP_CMD.PROC',
-         'stop_val': 1,
-         'done': 'XF:23IDA-OP:1{Mir:1}BUSY_STS',
-         'done_val': 0,
-         'name': 'm1a_z',
-        })
+#args = ('XF:23IDA-OP:1{Mir:1-Ax:Z}Mtr_POS_SP',
+#        {'readback': 'XF:23IDA-OP:1{Mir:1-Ax:Z}Mtr_MON',
+#         'act': 'XF:23IDA-OP:1{Mir:1}MOVE_CMD.PROC',
+#         'act_val': 1,
+#         'stop': 'XF:23IDA-OP:1{Mir:1}STOP_CMD.PROC',
+#         'stop_val': 1,
+#         'done': 'XF:23IDA-OP:1{Mir:1}BUSY_STS',
+#         'done_val': 0,
+#         'name': 'm1a_z',
+#        })
 #m1a_z = PVPositioner(args[0], **args[1])
 
 # AreaDetector crud

ophyd/runengine/runengine.py
@@ -35,7 +35,9 @@ def _get_info(positioners=None, detectors=None, data=None):
     def get_det_info(detector):
         """Internal function to grab info from a detector
         """
-        val = np.asarray(data[detector.name])
+        # grab 'value' from [value, timestamp]
+        val = np.asarray(data[detector.name][0]) 
+
         dtype = 'number'
         try:
             shape = val.shape
@@ -43,7 +45,8 @@ def _get_info(positioners=None, detectors=None, data=None):
             # val is probably a float...
             shape = None
         source = "PV:{}".format(detector.pvname)
-        if not shape:
+
+        if shape:
             dtype = 'array'
         return {detector.name: {'source': source, 'dtype': dtype,
                                 'shape': shape}}