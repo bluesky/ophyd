fe07b451c5a24a37f89902d7a28cb5246bad123f
Stuart B. Wilkins <swilkins@bnl.gov>
2015-02-21 12:10:07 -0500

    ENH : Added detector class and scaler in new model

--- ophyd/controls/scaler.py
+++ ophyd/controls/scaler.py
@@ -2,14 +2,13 @@ from __future__ import print_function
 import logging
 import time
 
-from .signal import (SignalGroup, EpicsSignal)
-from ..utils.epics_pvs import record_field
-
+from .signal import EpicsSignal
+from .detector import Detector
 
 logger = logging.getLogger(__name__)
 
 
-class EpicsScaler(SignalGroup):
+class EpicsScaler(Detector):
     '''SynApps Scaler Record interface
 
     Parameters
@@ -23,9 +22,7 @@ class EpicsScaler(SignalGroup):
     def __init__(self, record, numchan=8, *args, **kwargs):
         self._record = record
         self._numchan = numchan
-
-        SignalGroup.__init__(self, *args, **kwargs)
-
+        super(EpicsScaler, self).__init__(*args, **kwargs)
         '''Which record fields do we need to expose here (minimally)?
 
         CNT     -- start/stop counting
@@ -38,32 +35,24 @@ class EpicsScaler(SignalGroup):
 
         Eventually need to provide PR1..16 -- preset counts too.
         '''
-        signals = [EpicsSignal(self.field_pv('CNT'),
+        signals = [EpicsSignal(self._field_pv('CNT'),
                                alias='_count_ctl',
                                name=''.join([self.name, '_cnt'])),
-                   EpicsSignal(self.field_pv('CONT'),
+                   EpicsSignal(self._field_pv('CONT'),
                                alias='_count_mode',
                                name=''.join([self.name, '_cont'])),
-                   EpicsSignal(self.field_pv('T'),
+                   EpicsSignal(self._field_pv('T'),
                                alias='_elapsed_time',
                                name=''.join([self.name, '_t'])),
-                   EpicsSignal(self.field_pv('TP'),
+                   EpicsSignal(self._field_pv('TP'),
                                alias='_preset_time',
                                name=''.join([self.name, '_preset']))
                    ]
 
-        # create the 'NM1..numchan' channel name Signals
-        ch_names = []
-        for ch in range(1, numchan + 1):
-            name = ''.join([self.field_pv('NM'), str(ch)])
-            ch_names.append(EpicsSignal(name,
-                            name=''.join([self.name, '_nm', str(ch)]),
-                            alias=''.join(['_ch', str(ch), '_name'])))
-        signals += ch_names
         # create the 'S1..numchan' channel count Signals (read-only)
         ch_names = []
         for ch in range(1, numchan + 1):
-            name = ''.join([self.field_pv('S'), str(ch)])
+            name = '{}{}'.format(self._field_pv('S'), ch)
             ch_names.append(EpicsSignal(name, rw=False,
                             name=''.join([self.name, '_s', str(ch)]),
                             alias=''.join(['_ch', str(ch), '_count'])))
@@ -72,6 +61,9 @@ class EpicsScaler(SignalGroup):
         for sig in signals:
             self.add_signal(sig)
 
+    def _field_pv(self, field):
+        return '{}.{}'.format(self._record, field)
+
     def __repr__(self):
         repr = ['record={0._record!r}'.format(self),
                 'numchan={0._numchan!r}'.format(self),
@@ -79,50 +71,41 @@ class EpicsScaler(SignalGroup):
 
         return self._get_repr(repr)
 
-    @property
-    def record(self):
-        '''The EPICS record name'''
-        return self._record
+    def configure(self, **kwargs):
+        """Configure Scaler
 
-    def field_pv(self, field):
-        '''Return a full PV from the field name'''
-        return record_field(self._record, field)
+        Configure the scaler by setting autocount to off. The state will
+        be restored by deconfigure
 
-    def start(self):
-        self._count_ctl.put(1, wait=False)
+        TODO: Could set acquisition time here
+        """
+        self._autocount = self._count_mode.value
+        self._count_mode.value = 0
 
-    # TODO: should this be a non-blocking write?
-    # TODO: should writes be non-blocking by default?
-    def stop(self):
-        self._count_ctl.value = 0
+    def deconfigure(self, **kwargs):
+        """Deconfigure Scaler
 
-    @property
-    def count(self):
-        return self._count_ctl
+        Reset thet autocount status
+        """
+        self._count_mode.value = self._autocount
 
-    @property
-    def count_mode(self):
-        return self._count_mode.value
+    def acquire(self, **kwargs):
+        """Start the scaler counting and return status
 
-    @count_mode.setter
-    def count_mode(self, mode):
-        self._count_mode.value = mode
+        Returns
+        -------
+        DetectorStatus : Status of detector
+        """
 
-    @property
-    def preset_time(self):
-        return self._preset_time.value
+        def done_counting(**kwargs):
+            self._done_acquiring()
 
-    @preset_time.setter
-    def preset_time(self, time):
-        self._preset_time.value = time
+        self._count_ctl.put(1, wait=False,
+                            callback=done_counting)
+        return Detector.acquire(self, **kwargs)
 
-    def read(self, channels=None):
+    def read(self, **kwargs):
         '''Trigger a counting period and return all or selected channels.
-
-        Parameters
-        ----------
-        channels
-            a tuple enumerating the channels to return.
 
         Returns
         -------
@@ -130,14 +113,11 @@ class EpicsScaler(SignalGroup):
             Where channel numbers are the keys and values are the counts,
             i.e., {channel_x: counts}
         '''
-        # Block waiting for counting to complete
-        self._count_ctl.put(1, wait=True)
+        channels = range(1, self._numchan + 1)
 
-        if channels is None:
-            channels = range(1, self._numchan + 1)
-
-        # TODO: super-F'ugly... Add synchronous 'gets' in symmetry
-        # with the sync-puts (put-completion)
-        time.sleep(0.005)
-
-        return {ch: getattr(self, '_ch%s_count' % ch).value for ch in channels}
+        rtn = {}
+        for ch in channels:
+            sig = getattr(self, '_ch{}_count'.format(ch))
+            rtn.update({sig.name: {'value': sig.value,
+                                  'timestamp': sig.timestamp}})
+        return rtn
