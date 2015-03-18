8f7ab1837372ff717fe0f6ebe3df6b0e6507a769
Garth Williams <gwilliams@bnl.gov>
2015-03-18 13:42:12 -0400

    remove unused collections package from runengine.py remove spurious comments in scan_api.py

--- ophyd/runengine/runengine.py
+++ ophyd/runengine/runengine.py
@@ -6,7 +6,6 @@ import time
 from threading import Thread
 from Queue import Queue
 import numpy as np
-import collections
 from ..session import register_object
 from ..controls.detector import Detector
 from ..controls.signal import SignalGroup
