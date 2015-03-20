b551aeee55a92a662fe83383bebb3c590d6f8f09
Daron Chabot <dchabot@bnl.gov>
2015-03-20 17:53:50 -0400

    Testing fsm code.

--- examples/runtest.py
+++ examples/runtest.py
@@ -9,6 +9,8 @@ def foo(msg='foo'):
 def bar(msg='bar'):
     print(msg)
 
+def baz(msg='baz'):
+    print(msg)
 
 run_num = 11
 
@@ -16,4 +18,5 @@ run = Run(run_num)
 
 run.trigger(foo, every=Run.BEGIN_RUN, msg='\n\n\tFoo msg...\n\n')
 run.trigger(bar, every=Run.END_RUN, msg='\n\n\tBar msg...\n\n')
+run.trigger(baz, every=Run.SCAN_EV, msg='\n\n\tBaz msg...\n\n')
 run.start()
