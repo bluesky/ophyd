d4a4eca802d2a5ea820bca03fe0b879abcf4cd08
Thomas A Caswell <tcaswell@bnl.gov>
2015-04-06 15:16:12 -0400

    API : remove `Scan` from the public API
    See b2e21ce39e796365e44f9a7e807d9933404a2321 for explaination.

--- ophyd/userapi/__init__.py
+++ ophyd/userapi/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 from .cli_api import (mov, movr, set_pos, wh_pos, set_lm, log_pos,
                      log_pos_diff, log_pos_mov)
 
-from .scan_api import (Scan, Count, AScan, DScan)
+from .scan_api import (Count, AScan, DScan)
