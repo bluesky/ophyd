0f1167035c5b0c236a4986f0169220b5dd107847
Daron Chabot <dchabot@bnl.gov>
2015-07-21 10:49:21 -0400

    WIP: addition of RunDocs

--- ophyd/runengine/run.py
+++ ophyd/runengine/run.py
@@ -14,18 +14,107 @@ import traceback
 
 from ..controls.ophydobj import OphydObject
 from .fsm import FSM, State
+#from ..session import session
+import random
 
 
 logging.basicConfig(log_level=logging.DEBUG)
 
+
+class Session(object):
+    def get_scan_id(self):
+        return random.randint(0, 10000)
+
+    def get_beamline_config(self):
+        return {}
+
+    def get_beamline_id(self):
+        return 'FUX'
+
+
+session = Session()
+
+
+class RunDocs(object):
+    def __init__(self, **kwargs):
+        super(RunDocs, self).__init__()
+        self.doc = {'uid': str(uuid.uuid4()), 'time': time.time()}
+
+    @classmethod
+    def begin_run(cls, scan_id=None, group=None, owner=None,
+                  beamline_config=None, beamline_id=None, **kwargs):
+        br_doc = cls().doc
+
+        if scan_id is None:
+            scan_id = session.get_scan_id()
+        br_doc['scan_id'] = scan_id
+
+        if group is None:
+            group = grp.getgrgid(os.getgid()).gr_name
+        br_doc['group'] = group
+
+        if owner is None:
+            owner = getpass.getuser()
+        br_doc['owner'] = owner
+
+        if beamline_config is None:
+            beamline_config = session.get_beamline_config()
+        br_doc['beamline_config'] = beamline_config
+
+        if beamline_id is None:
+            beamline_id = session.get_beamline_id()
+        br_doc['beamline_id'] = beamline_id
+
+        if kwargs:
+            br_doc.update(kwargs)
+
+        return br_doc
+
+    @classmethod
+    def end_run(cls, br_event, state, reason=None):
+        er_doc = cls().doc
+
+        er_doc['begin_run_event'] = br_event['uid']
+
+        if state in ('success', 'abort', 'fail'):
+            er_doc['completion_state'] = state
+        else:
+            raise ValueError('Completion state must be one of success|abort|fail')
+
+        if reason is not None:
+            er_doc['reason'] = reason
+
+        return er_doc
+
+    @classmethod
+    def descriptor(cls, br_event, sources, **kwargs):
+        desc = cls().doc
+
+        desc['begin_run_event'] = br_event['uid']
+        desc['keys'] = {src.name: src.describe() for src in sources}
+        if kwargs:
+            desc.update(kwargs)
+
+        return desc
+
+    @classmethod
+    def event(cls, desc, seq_num, elems):
+        event = cls().doc
+
+        event['descriptor'] = desc['uid']
+        event['seq_num'] = seq_num
+        event['data'] = {k.name: v for k,v in elems.iteritems()}
+
+        return event
+
+
 class Event(object):
-    def __init__(self, fcn, name=None):
+    def __init__(self, fcn, run, name=None):
         self.__name__ = name
+        self._run = run
+        self._dq = Queue.Queue()
         self.seq_num = 0
-        self.uuid = str(uuid.uuid4())
         self.desc = None # event descriptor
-        self.time = None
-        self.data = None
         self.fcn = fcn
         # scaler_events are 2-tuples - (fcn, scale)
         self.scaler_events = []
@@ -64,6 +153,10 @@ class Event(object):
         self.seq_num += 1
 
         return ret
+
+    def save(self, elems):
+        vals = {e: e.read() for e in elems}
+        self._dq.put(vals)
 
 
 class Run(FSM, OphydObject):
@@ -125,7 +218,8 @@ class Run(FSM, OphydObject):
                 self._thread_ev.clear()
             [thread.start() for thread in self._threads.itervalues()]
         else:
-            self.stop()
+            # given nothing to do
+            self.stop(status='success')
 
     def _stop_threads(self, *args, **kwargs):
         if self._threads:
@@ -134,17 +228,20 @@ class Run(FSM, OphydObject):
     def _begin_run(self, *args, **kwargs):
         # TODO: emit begin_run_event
         print('\n\nbegin_run_event\n\n')
+        self.begin_run_event = RunDocs.begin_run()
+        print(self.begin_run_event)
 
-    def _end_run(self, status=None, **kwargs):
+    def _end_run(self, status=None, reason=None, **kwargs):
         # TODO: emit end_run_event
         print('\n\nend_run_event\n\n')
-        if status:
-            print('status =', status)
+        self.end_run_event = RunDocs.end_run(self.begin_run_event, status,
+                                             reason=reason)
+        print(self.end_run_event)
 
-    def _trajectory_scan(self, scan):
+    def _trajectory_scan(self, scan, **kwargs):
         for state in scan:
             if not self._thread_ev.is_set():
-                scan.next_state(scan)
+                scan.next_state(scan, **kwargs)
             else:
                 return
 
@@ -154,7 +251,7 @@ class Run(FSM, OphydObject):
 
     def _add_event(self, ev_type, cb):
         # NOTE - cb is a Scan object here
-        event = Event(self._trajectory_scan, name=ev_type)
+        event = Event(self._trajectory_scan, self, name=ev_type)
         thread = ScanContext(event, cb, self, name=ev_type)
         self._events[event.__name__] = event
         self._threads[thread.name] = thread
@@ -190,7 +287,7 @@ class Run(FSM, OphydObject):
             scale = int(scale)
 
             # add the callback to the Event to be scaled
-            cb = Event(cb)
+            cb = Event(cb, self)
             scaled_event = self._events[event]
             scaled_event.scaler_events.append((cb, scale))
             self._events[cb.__name__] = cb
@@ -201,9 +298,9 @@ class Run(FSM, OphydObject):
  
 
 class ScanContext(object):
-    def __init__(self, fcn, scan, run, **kwargs):
+    def __init__(self, event, scan, run, **kwargs):
         self.name = kwargs.pop('name')
-        self._fcn = fcn
+        self._event = event 
         self._scan = scan
         self._run = run
 
@@ -220,7 +317,7 @@ class ScanContext(object):
 
         while not self._run._thread_ev.is_set():
             try:
-                self._fcn(scan)
+                self._event(scan, event=self._event)
             except StopIteration as si:
                 exc = traceback.format_exc()
                 logging.debug('%s', exc)
