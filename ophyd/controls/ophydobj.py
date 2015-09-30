83d3b11903dadc10273da024bc4003cd0c1bdbea
danielballan <daniel.b.allan@gmail.com>
2015-09-30 17:06:46 -0400

    ENH: Add pass-through configure/deconfigure methods to OphydObj.

--- ophyd/controls/ophydobj.py
+++ ophyd/controls/ophydobj.py
@@ -323,6 +323,13 @@ class OphydObject(object):
     def state(self):
         return {}
 
+    def configure(self, state=None):
+        # does nothing; subclasses can override if configuration is possible
+        return self.state, self.state
+
+    def deconfigure(self):
+        return self.state
+
     def __repr__(self):
         return self._get_repr()
 
