616f3083a2e30ef54c3b12b8a227931c67c127a6
K Lauer <klauer@bnl.gov>
2015-11-16 12:56:29 -0500

    CLN: further clean interactive example

--- examples/interactive.py
+++ examples/interactive.py
@@ -4,12 +4,8 @@ import config
 import time
 
 from ophyd.controls.positioner import EpicsMotor
-import ophyd.commands
 from ophyd.commands import (mov, movr, set_pos, wh_pos, set_lm, log_pos,
                             log_pos_diff)
-
-import IPython
-ip = IPython.get_ipython()
 
 
 rec1, rec2, rec3 = config.motor_recs[:3]
@@ -21,17 +17,22 @@ m3 = EpicsMotor(rec3)
 # wait for positioners to connect...
 time.sleep(1.0)
 
+print()
 print('Moving m1 to 0.3:')
 mov(m1, 0.3)
 
+print()
 print('Moving m1 to 0.0:')
 mov(m1, 0.0)
 
+print()
 print('Moving m1 to 0.3 (relative):')
 movr(m1, 0.3)
 
+print()
 print('wh_pos getting all positioners available, if running in IPython:')
 wh_pos()
 
+print()
 print('wh_pos of a single motor:')
 wh_pos([m1])
