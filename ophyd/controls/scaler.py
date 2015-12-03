b472efb88bb84f2438bbb7999d71676d03e5dd7b
K Lauer <klauer@bnl.gov>
2015-12-03 12:35:35 -0500

    MNT: raise_if_disconnected taken care of by device

--- ophyd/controls/scaler.py
+++ ophyd/controls/scaler.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 import logging
 
 from .signal import (EpicsSignal, EpicsSignalRO)
-from ..utils.epics_pvs import raise_if_disconnected
 from .device import OphydDevice
 from .device import (Component as C, DynamicComponent as DC)
 
@@ -23,31 +22,18 @@ class EpicsScaler(OphydDevice):
     # gates = DC('', DC.make_def(EpicsSignalRO, '.G{index:d}')
 
     @property
-    @raise_if_disconnected
-    def count_time(self):
-        return self.preset_time.value
-
-    @count_time.setter
-    @raise_if_disconnected
-    def count_time(self, val):
-        self.preset_time.put(val)
-
-    @property
-    @raise_if_disconnected
     def auto_count(self):
         """Return the autocount status"""
-        return (self.count_mode.value == 1)
+        return (self.count_mode.get() == 1)
 
     @auto_count.setter
-    @raise_if_disconnected
     def auto_count(self, val):
         """Set the autocount status"""
         if val:
-            self.count_mode.value = 1
+            self.count_mode.put(1)
         else:
-            self.count_mode.value = 0
+            self.count_mode.put(0)
 
-    @raise_if_disconnected
     def configure(self, state=None):
         """Configure Scaler
 
@@ -56,13 +42,12 @@ class EpicsScaler(OphydDevice):
         """
         if state is None:
             state = {}
-        self._autocount = self._count_mode.value
-        self.count_mode.value = 0
+        self._old_count_mode = self._count_mode.get()
+        self.count_mode.put(0)
 
-    @raise_if_disconnected
     def deconfigure(self):
         """Deconfigure Scaler
 
         Reset the autocount status
         """
-        self.count_mode.value = self._autocount
+        self.count_mode.put(self._old_count_mode)
