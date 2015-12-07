6a44abb1da36006c2de32f4a98990dc49d1c8db2
K Lauer <klauer@bnl.gov>
2015-12-07 13:54:17 -0500

    TST: add travis, coveragerc and fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import (setup, find_packages)
 import versioneer
 
 
@@ -7,8 +7,4 @@ setup(name='ophyd',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       license='BSD',
-      packages=['ophyd',
-                'ophyd.controls',
-                'ophyd.controls.areadetector',
-                'ophyd.utils',
-                'ophyd.tests'])
+      packages=find_packages())
