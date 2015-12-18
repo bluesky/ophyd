a60cb6162d80569d246e972311033ce7fc4dbcdd
danielballan <daniel.b.allan@gmail.com>
2015-12-18 15:16:17 -0500

    CLN: No need for DetectorBase

--- ophyd/controls/areadetector/detectors.py
+++ ophyd/controls/areadetector/detectors.py
@@ -173,100 +173,100 @@ class MultiTrigger(TriggerBase):
             self._run_subs(sub_type=self._SUB_ACQ_DONE)
 
 
-class AreaDetector(DetectorBase):
+class AreaDetector(ADBase):
     cam = C(cam.AreaDetectorCam, 'cam1:')
 
 
-class SimDetector(DetectorBase):
+class SimDetector(ADBase):
     _html_docs = ['simDetectorDoc.html']
     cam = C(cam.SimDetectorCam, 'cam1:')
 
 
-class AdscDetector(DetectorBase):
+class AdscDetector(ADBase):
     _html_docs = ['adscDoc.html']
     cam = C(cam.AdscDetectorCam, 'cam1:')
 
 
-class AndorDetector(DetectorBase):
+class AndorDetector(ADBase):
     _html_docs = ['andorDoc.html']
     cam = C(cam.AndorDetectorCam, 'cam1:')
 
 
-class Andor3Detector(DetectorBase):
+class Andor3Detector(ADBase):
     _html_docs = ['andor3Doc.html']
     cam = C(cam.Andor3DetectorCam, 'cam1:')
 
 
-class BrukerDetector(DetectorBase):
+class BrukerDetector(ADBase):
     _html_docs = ['BrukerDoc.html']
     cam = C(cam.Andor3DetectorCam, 'cam1:')
 
 
-class FirewireLinDetector(DetectorBase):
+class FirewireLinDetector(ADBase):
     _html_docs = ['FirewireWinDoc.html']
     cam = C(cam.FirewireLinDetectorCam, 'cam1:')
 
 
-class FirewireWinDetector(DetectorBase):
+class FirewireWinDetector(ADBase):
     _html_docs = ['FirewireWinDoc.html']
     cam = C(cam.FirewireWinDetectorCam, 'cam1:')
 
 
-class LightFieldDetector(DetectorBase):
+class LightFieldDetector(ADBase):
     _html_docs = ['LightFieldDoc.html']
     cam = C(cam.LightFieldDetectorCam, 'cam1:')
 
 
-class Mar345Detector(DetectorBase):
+class Mar345Detector(ADBase):
     _html_docs = ['Mar345Doc.html']
     cam = C(cam.Mar345DetectorCam, 'cam1:')
 
 
-class MarCCDDetector(DetectorBase):
+class MarCCDDetector(ADBase):
     _html_docs = ['MarCCDDoc.html']
     cam = C(cam.MarCCDDetectorCam, 'cam1:')
 
 
-class PerkinElmerDetector(DetectorBase):
+class PerkinElmerDetector(ADBase):
     _html_docs = ['PerkinElmerDoc.html']
     cam = C(cam.LightFieldDetectorCam, 'cam1:')
 
 
-class PSLDetector(DetectorBase):
+class PSLDetector(ADBase):
     _html_docs = ['PSLDoc.html']
     cam = C(cam.PSLDetectorCam, 'cam1:')
 
 
-class PilatusDetector(DetectorBase):
+class PilatusDetector(ADBase):
     _html_docs = ['pilatusDoc.html']
     cam = C(cam.PilatusDetectorCam, 'cam1:')
 
 
-class PixiradDetector(DetectorBase):
+class PixiradDetector(ADBase):
     _html_docs = ['PixiradDoc.html']
     cam = C(cam.PixiradDetectorCam, 'cam1:')
 
 
-class PointGreyDetector(DetectorBase):
+class PointGreyDetector(ADBase):
     _html_docs = ['PointGreyDoc.html']
     cam = C(cam.PointGreyDetectorCam, 'cam1:')
 
 
-class ProsilicaDetector(DetectorBase):
+class ProsilicaDetector(ADBase):
     _html_docs = ['prosilicaDoc.html']
     cam = C(cam.ProsilicaDetectorCam, 'cam1:')
 
 
-class PvcamDetector(DetectorBase):
+class PvcamDetector(ADBase):
     _html_docs = ['pvcamDoc.html']
     cam = C(cam.PvcamDetectorCam, 'cam1:')
 
 
-class RoperDetector(DetectorBase):
+class RoperDetector(ADBase):
     _html_docs = ['RoperDoc.html']
     cam = C(cam.RoperDetectorCam, 'cam1:')
 
 
-class URLDetector(DetectorBase):
+class URLDetector(ADBase):
     _html_docs = ['URLDoc.html']
     cam = C(cam.URLDetectorCam, 'cam1:')
