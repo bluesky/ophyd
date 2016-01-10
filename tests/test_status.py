26bafe7d3eabe374833fb1dd6b9eebdc64daa9c2
Thomas A Caswell <tcaswell@bnl.gov>
2016-01-10 13:08:34 -0500

    TST: stand-alone of StatusBase

---
+++ tests/test_status.py
@@ -0,0 +1,33 @@
+from ophyd.controls.ophydobj import StatusBase
+
+
+def _setup_st():
+    st = StatusBase()
+    state = {}
+
+    def cb():
+        state['done'] = True
+
+    return st, state, cb
+
+
+def test_status_post():
+    st, state, cb = _setup_st()
+
+    assert 'done' not in state
+    st.finished_cb = cb
+    assert 'done' not in state
+    st._finished()
+    assert 'done' in state
+    assert state['done']
+
+
+def test_status_pre():
+    st, state, cb = _setup_st()
+
+    st._finished()
+
+    assert 'done' not in state
+    st.finished_cb = cb
+    assert 'done' in state
+    assert state['done']
