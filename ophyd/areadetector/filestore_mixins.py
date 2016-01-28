d0d64af151a530902eb29a87400c9c1cf60bb236
Thomas A Caswell <tcaswell@bnl.gov>
2016-01-28 16:13:00 -0500

    FIX: Do not set stage sigs in stage unless we must

--- ophyd/areadetector/filestore_mixins.py
+++ ophyd/areadetector/filestore_mixins.py
@@ -142,23 +142,28 @@ class FileStoreBase(BlueskyInterface, GenerateDatumInterface):
 
 
 class FileStoreHDF5(FileStoreBase):
-    def stage(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.stage_sigs.update([(self.file_template, '%s%s_%6.6d.h5'),
                                 (self.file_write_mode, 'Stream'),
                                 (self.capture, 1)
                                 ])
+    def stage(self):
         super().stage()
         res_kwargs = {'frame_per_point': self.num_captured.get()}
         self._resource = fs.insert_resource('AD_HDF5', self._fn, res_kwargs)
 
 
 class FileStoreTIFF(FileStoreBase):
-    def stage(self):
-        # 'Single' means one image : one file. It does NOT mean that
-        # 'num_images' is ignored.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.stage_sigs.update([(self.file_template, '%s%s_%6.6d.tiff'),
                                 (self.file_write_mode, 'Single'),
                                 ])
+        # 'Single' file_write_mode means one image : one file.
+        # It does NOT mean that 'num_images' is ignored.
+
+    def stage(self):
         super().stage()
         res_kwargs = {'template': self.file_template.get(),
                       'filename': self.file_name.get(),
@@ -175,14 +180,8 @@ class FileStoreTIFFSquashing(FileStoreBase):
         self._num_sets_name = number_of_sets_name
         self._cam_name = cam_name
         self._proc_name = proc_name
-
-    def stage(self):
-        # 'Single' means one image : one file. It does NOT mean that
-        # 'num_images' is ignored.
         cam = getattr(self.parent, self._cam_name)
         proc = getattr(self.parent, self._proc_name)
-        images_per_set = getattr(self.parent, self._ips_name).get()
-        num_sets = getattr(self.parent, self._num_sets_name).get()
         self.stage_sigs.update([(self.file_template, '%s%s_%6.6d.tiff'),
                                 (self.file_write_mode, 'Single'),
                                 (proc.nd_array_port, cam.port_name.get()),
@@ -191,11 +190,21 @@ class FileStoreTIFFSquashing(FileStoreBase):
                                 (proc.filter_type, 'Average'),
                                 (proc.auto_reset_filter, 1),
                                 (proc.filter_callbacks, 1),
-                                (proc.num_filter, images_per_set),
-                                (cam.num_images, images_per_set * num_sets),
                                 (self.nd_array_port, proc.port_name.get())
                                 ])
+        # 'Single' file_write_mode means one image : one file.
+        # It does NOT mean that 'num_images' is ignored.
+
+    def stage(self):
+        cam = getattr(self.parent, self._cam_name)
+        proc = getattr(self.parent, self._proc_name)
+        images_per_set = getattr(self.parent, self._ips_name).get()
+        num_sets = getattr(self.parent, self._num_sets_name).get()
+
+        self.stage_sigs.update([(proc.num_filter, images_per_set),
+                                (cam.num_images, images_per_set * num_sets)])
         super().stage()
+
         res_kwargs = {'template': self.file_template.get(),
                       'filename': self.file_name.get(),
                       'frame_per_point': num_sets}
