4a62f958e6ad17fd539302e5091a16b67e7172a4
K Lauer <klauer@bnl.gov>
2016-02-23 14:51:35 -0500

    MNT: signal example cleanup

--- examples/signal.py
+++ examples/signal.py
@@ -8,42 +8,29 @@ from ophyd import (EpicsSignal, Signal)
 from ophyd.utils.epics_pvs import record_field
 
 
-def test():
-    def callback(sub_type=None, timestamp=None, value=None, **kwargs):
-        logger.info('[callback] [%s] (type=%s) value=%s', timestamp, sub_type,
-                    value)
+def callback(sub_type=None, timestamp=None, value=None, **kwargs):
+    logger.info('[callback] [%s] (type=%s) value=%s', timestamp, sub_type,
+                value)
 
-        # Test that the monitor dispatcher works (you cannot use channel access in
-        # callbacks without it)
-        logger.info('[callback] caget=%s' % rw_signal.get())
+    # Test that the monitor dispatcher works (you cannot use channel access in
+    # callbacks without it)
+    logger.info('[callback] caget=%s' % rw_signal.get())
 
-    loggers = ('ophyd.signal',
-               )
+logger = config.logger
 
-    config.setup_loggers(loggers)
-    logger = config.logger
+motor_record = config.motor_recs[0]
+val = record_field(motor_record, 'VAL')
+rbv = record_field(motor_record, 'RBV')
 
-    motor_record = config.motor_recs[0]
-    val = record_field(motor_record, 'VAL')
-    rbv = record_field(motor_record, 'RBV')
+rw_signal = EpicsSignal(rbv, write_pv=val)
+rw_signal.subscribe(callback, event_type=rw_signal.SUB_VALUE)
+rw_signal.subscribe(callback, event_type=rw_signal.SUB_SETPOINT)
 
-    rw_signal = EpicsSignal(rbv, write_pv=val)
-    rw_signal.subscribe(callback, event_type=rw_signal.SUB_VALUE)
-    rw_signal.subscribe(callback, event_type=rw_signal.SUB_SETPOINT)
+rw_signal.value = 2
+time.sleep(1.)
+rw_signal.value = 1
+time.sleep(1.)
 
-    rw_signal.value = 2
-    time.sleep(1.)
-    rw_signal.value = 1
-    time.sleep(1.)
-
-    # You can also create a Python Signal:
-    sig = Signal(name='testing', value=10)
-    logger.info('Python signal: %s' % sig)
-
-    # Even one with a separate setpoint/readback value:
-    sig = Signal(name='testing', value=10, setpoint=2,
-                 separate_readback=True)
-    logger.info('Python signal: %s' % sig)
-
-if __name__ == '__main__':
-    test()
+# You can also create a Python Signal:
+sig = Signal(name='testing', value=10)
+logger.info('Python signal: %s' % sig)
