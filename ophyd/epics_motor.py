5151832cd86aa6064d720fa922aac3480ee424e9
hhslepicka <hhslepicka@gmail.com>
2016-02-26 15:31:36 -0500

    ENH: Positioner/EpicsMotor Additions Partially Fix Issue: #273 - [x] Homing - [x] Calibration - set_current_position - [x] Limit Switch Status - [x] Velocity - [x] Acceleration - [x] Offset and Offset Freeze - [x] check the limit switches on the _move_changed prior to invoke _done_moving. - [x] Tests

--- ophyd/epics_motor.py
+++ ophyd/epics_motor.py
@@ -18,9 +18,15 @@ from .utils.epics_pvs import raise_if_disconnected
 from .positioner import PositionerBase
 from .device import (Device, Component as Cpt)
 from .status import wait as status_wait
+from enum import Enum
 
 
 logger = logging.getLogger(__name__)
+
+
+class HomeEnum(str, Enum):
+    forward = "forward"
+    reverse = "reverse"
 
 
 class EpicsMotor(Device, PositionerBase):
@@ -42,12 +48,23 @@ class EpicsMotor(Device, PositionerBase):
     settle_time : float, optional
         The amount of time to wait after moves to report status completion
     '''
+    user_offset = Cpt(EpicsSignal, '.OFF')
     user_readback = Cpt(EpicsSignalRO, '.RBV')
     user_setpoint = Cpt(EpicsSignal, '.VAL', limits=True)
     motor_egu = Cpt(EpicsSignal, '.EGU')
     motor_is_moving = Cpt(EpicsSignalRO, '.MOVN')
     motor_done_move = Cpt(EpicsSignalRO, '.DMOV')
     motor_stop = Cpt(EpicsSignal, '.STOP')
+    motor_offset_freeze_switch = Cpt(EpicsSignal, '.FOFF')
+    motor_velocity = Cpt(EpicsSignal, '.VELO')
+    motor_acceleration = Cpt(EpicsSignal, '.ACCL')
+    motor_set_use_switch = Cpt(EpicsSignal, '.SET')
+    motor_at_high_limit_switch = Cpt(EpicsSignal, '.HLS')
+    motor_at_low_limit_switch = Cpt(EpicsSignal, '.LLS')
+    motor_home_forward = Cpt(EpicsSignal, '.HOMF')
+    motor_home_reverse = Cpt(EpicsSignal, '.HOMR')
+    motor_direction_of_travel = Cpt(EpicsSignal, '.TDIR')
+
 
     def __init__(self, prefix, *, read_attrs=None, configuration_attrs=None,
                  monitor_attrs=None, name=None, parent=None, **kwargs):
@@ -129,6 +146,93 @@ class EpicsMotor(Device, PositionerBase):
         '''
         return self._position
 
+    @property
+    @raise_if_disconnected
+    def offset(self):
+        '''The current user offset value of the motor in its engineering units
+
+        Returns
+        -------
+        offset : float
+        '''
+        return self.user_offset.get()
+
+    @property
+    @raise_if_disconnected
+    def offset_frozen(self):
+        '''Whether or not the motor offset is frozen
+
+        Returns
+        -------
+        frozen : bool
+        '''
+        return bool(self.motor_offset_freeze_switch.get())
+
+    @property
+    @raise_if_disconnected
+    def at_high_limit_switch(self):
+        '''Whether or not the hardware high limit switch is active
+
+        Returns
+        -------
+        active : bool
+        '''
+        return bool(self.motor_at_high_limit_switch.get())
+
+    @property
+    @raise_if_disconnected
+    def at_low_limit_switch(self):
+        '''Whether or not the hardware low limit switch is active
+
+        Returns
+        -------
+        active : bool
+        '''
+        return bool(self.motor_at_low_limit_switch.get())
+
+    @raise_if_disconnected
+    def set_current_position(self, pos):
+        '''Configure the motor user position to the given value
+
+        Parameters
+        ----------
+        pos
+           Position to set.
+
+        '''
+        self.motor_set_use_switch.put(1, wait=True)
+        self.user_setpoint.put(pos, wait=True)
+        self.motor_set_use_switch.put(0, wait=True)
+
+    @raise_if_disconnected
+    def home(self, direction, wait=True, **kwargs):
+        '''Perform the default homing function in the desired direction
+
+        Parameters
+        ----------
+        direction : HomeEnum
+           Direction in which to perform the home search.
+        '''
+        direction = HomeEnum(direction)
+
+        self._started_moving = False
+        position = (self.low_limit+self.high_limit)/2
+        status = super().move(position, **kwargs)
+
+        if direction == HomeEnum.forward:
+            self.motor_home_forward.put(1, wait=False)
+        else:
+            self.motor_home_reverse.put(1, wait=False)
+
+        try:
+            if wait:
+                status_wait(status)
+        except KeyboardInterrupt:
+            self.stop()
+            raise
+
+        return status
+
     def check_value(self, pos):
         '''Check that the position is within the soft limits'''
         self.user_setpoint.check_value(pos)
@@ -155,7 +259,17 @@ class EpicsMotor(Device, PositionerBase):
                            value=value, **kwargs)
 
         if was_moving and not self._moving:
-            self._done_moving(timestamp=timestamp, value=value)
+            success = True
+            # Check if we are moving towards the low limit switch
+            if self.motor_direction_of_travel.get() == 0:
+                if self.at_low_limit_switch:
+                    success = False
+            # No, we are going to the high limit switch
+            else:
+                if self.at_high_limit_switch:
+                    success = False
+
+            self._done_moving(success=success, timestamp=timestamp, value=value)
 
     @property
     def report(self):
