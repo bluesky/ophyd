fed1d3ec95e7d8141c70b9f39e827eeceb35d8f2
Thomas A Caswell <tcaswell@bnl.gov>
2017-10-18 11:38:32 -0400

    MNT: make warning more useful

--- ophyd/ophydobj.py
+++ ophyd/ophydobj.py
@@ -203,7 +203,7 @@ class OphydObject:
         if event_type is None:
             warnings.warn("Please specify which call back you wish to "
                           "attach to defaulting to {}"
-                          .format(self._default_sub))
+                          .format(self._default_sub), stacklevel=2)
             event_type = self._default_sub
 
         if event_type is None:
