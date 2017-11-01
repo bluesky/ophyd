ed007bbe35b8d787487d1be0eb9039b761d03450
Thomas A Caswell <tcaswell@bnl.gov>
2017-10-31 21:53:15 -0400

    ENH: use more general / understandable ENV to control control layer

--- ophyd/control_layer.py
+++ ophyd/control_layer.py
@@ -5,8 +5,11 @@ __all__ = ['setup',
            'get_pv',
            'pv_form',
            'thread_class']
+_cl = os.environ.get('OPHYD_CONTROL_LAYER', '').lower()
 
-if os.environ.get('CAPROTO', False):
+if _cl == 'caproto':
     from ._caproto_shim import setup, caput, caget, get_pv, pv_form, thread_class
 else:
     from ._pyepics_shim import setup, caput, caget, get_pv, pv_form, thread_class
+
+del _cl
