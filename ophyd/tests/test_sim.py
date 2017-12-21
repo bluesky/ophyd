66a0adcaf121197f318e0dc76decf5a862be2a4a
Thomas A Caswell <tcaswell@bnl.gov>
2017-12-21 11:43:48 -0500

    STY: whitespace

--- ophyd/tests/test_sim.py
+++ ophyd/tests/test_sim.py
@@ -33,14 +33,14 @@ def test_random_state_gauss2d():
     """With given random state, the output value should stay the same.
     Test performs on 2D gaussian.
     """
-    dlist=  []
+    dlist = []
     motor1 = SynAxis(name='motor1')
     motor2 = SynAxis(name='motor2')
     for i in range(2):
         s = np.random.RandomState(0)
         noisy_det = Syn2DGauss('noisy_det', motor1, 'motor1', motor2, 'motor2',
-                                center=(0, 0), Imax=1, noise='uniform',
-                                sigma=1, noise_multiplier=0.1, random_state=s)
+                               center=(0, 0), Imax=1, noise='uniform',
+                               sigma=1, noise_multiplier=0.1, random_state=s)
         noisy_det.trigger()
         d = noisy_det.read()['noisy_det']['value']
         dlist.append(d)
