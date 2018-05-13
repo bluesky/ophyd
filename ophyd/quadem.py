bluesky/ophyd

commit cd4d65e33885e1382a514abf3646ea1f4920aa99

API: Make kind opt-out instead of opt-in.

ophyd/quadem.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 
 from . import (EpicsSignalRO, EpicsSignal, Component as Cpt,
                DynamicDeviceComponent as DDCpt, Signal,
-               Kind, RESPECT_KIND, kind_context)
+               Kind, kind_context)
 from .areadetector import (ADComponent as ADCpt, EpicsSignalWithRBV,
                            ImagePlugin, StatsPlugin, DetectorBase,
                            SingleTrigger)
@@ -20,8 +20,6 @@ def _current_fields(attr_base, field_base, range_, **kwargs):
 
 
 class QuadEM(SingleTrigger, DetectorBase):
-    _default_configuration_attrs = RESPECT_KIND
-    _default_read_attrs = RESPECT_KIND
     _status_type = DeviceStatus  # overrriding the default in SingleTrigger
 
     # This is needed because ophyd verifies that it can see all

ophyd/__init__.py
@@ -68,7 +68,7 @@ from .pseudopos import (PseudoPositioner, PseudoSingle)
 from .scaler import EpicsScaler
 from .device import (Device, Component, FormattedComponent,
                      DynamicDeviceComponent,
-                     RESPECT_KIND, kind_context)
+                     ALL_COMPONENTS, kind_context)
 from .status import StatusBase
 from .mca import EpicsMCA, EpicsDXP
 from .quadem import QuadEM, NSLS_EM, TetrAMM, APS_EM

ophyd/device.py
@@ -18,7 +18,7 @@ from collections.abc import MutableSequence
 from itertools import groupby
 
 A, B = TypeVar('A'), TypeVar('B')
-RESPECT_KIND = object()
+ALL_COMPONENTS = object()
 
 
 class OrderedDictType(Dict[A, B]):
@@ -749,23 +749,33 @@ class Device(BlueskyInterface, OphydObject, metaclass=ComponentMeta):
 
         super().__init__(name=name, parent=parent, kind=kind, **kwargs)
 
+        # The logic of these if blocks is:
+        # - If the user did not provide read_attrs, fall back on the default
+        # specified by the class, which ultimately falls back to Device, if no
+        # subclass overrides it. Either way, we now have a read_attrs.
+        # - If it is set to the sentinel ALL_COMPONENTS, ignore whatever the
+        # 'kind' settings are; just include everything. This is an escape catch
+        # for getting what _default_read_attrs=None used to do before 'kind'
+        # was implemented.
+        # - If it is set to a list, ignore whatever the 'kind' settings are and
+        # just include that list.
+        # - If it is set to None, respect whatever the 'kind' settings of the
+        # components are.
+
         # If any sub-Devices are to be removed from configuration_attrs and
         # read_attrs, we have to remove them from read_attrs first, or they
         # will not allow themselves to be removed from configuration_attrs.
-        if self._default_read_attrs is not RESPECT_KIND:
-            if read_attrs is None:
-                read_attrs = (self._default_read_attrs if
-                              self._default_read_attrs is not None
-                              else self.component_names)
+        if read_attrs is None:
+            read_attrs = self._default_read_attrs
+        if read_attrs is ALL_COMPONENTS:
+            read_attrs = self.component_names
         if read_attrs is not None:
             self.read_attrs = list(read_attrs)
 
-        if self._default_configuration_attrs is not RESPECT_KIND:
-            if configuration_attrs is None:
-                dflt_c_attrs = self._default_configuration_attrs
-                configuration_attrs = (dflt_c_attrs if
-                                       dflt_c_attrs is not None
-                                       else [])
+        if configuration_attrs is None:
+            configuration_attrs = self._default_configuration_attrs
+        if configuration_attrs is ALL_COMPONENTS:
+            configuration_attrs = self.component_names
         if configuration_attrs is not None:
             self.configuration_attrs = list(configuration_attrs)
 

ophyd/epics_motor.py
@@ -6,7 +6,7 @@ from .signal import (EpicsSignal, EpicsSignalRO)
 from .utils import DisconnectedError
 from .utils.epics_pvs import (raise_if_disconnected, AlarmSeverity)
 from .positioner import PositionerBase
-from .device import (Device, Component as Cpt, RESPECT_KIND)
+from .device import (Device, Component as Cpt)
 from .status import wait as status_wait
 from enum import Enum
 
@@ -40,9 +40,6 @@ class EpicsMotor(Device, PositionerBase):
     timeout : float, optional
         The default timeout to use for motion requests, in seconds.
     '''
-    _default_read_attrs = RESPECT_KIND
-    _default_configuration_attrs = RESPECT_KIND
-
     # position
     user_readback = Cpt(EpicsSignalRO, '.RBV', kind='hinted')
     user_setpoint = Cpt(EpicsSignal, '.VAL', limits=True)
@@ -285,5 +282,4 @@ class MotorBundle(Device):
 
     This provides better default behavior for ``hints``.
     """
-    _default_read_attrs = RESPECT_KIND
-    _default_configuration_attrs = RESPECT_KIND
+    ...

ophyd/mca.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 from .status import DeviceStatus
 from .signal import (Signal, EpicsSignal, EpicsSignalRO)
 from .device import (Device, Component as C, DynamicDeviceComponent as DDC,
-                     Staged, BlueskyInterface, RESPECT_KIND, Kind)
+                     Staged, BlueskyInterface, ALL_COMPONENTS, Kind)
 from .areadetector import EpicsSignalWithRBV as SignalWithRBV
 
 
@@ -60,9 +60,6 @@ def add_rois(range_, **kwargs):
 
 class EpicsMCARecord(Device):
     '''SynApps MCA Record interface'''
-    _default_configuration_attrs = RESPECT_KIND
-    _default_read_attrs = RESPECT_KIND
-
     stop_signal = C(EpicsSignal, '.STOP', kind='omitted')
     preset_real_time = C(EpicsSignal, '.PRTM', kind=Kind.CONFIG | Kind.NORMAL)
     preset_live_time = C(EpicsSignal, '.PLTM', kind='omitted')
@@ -73,9 +70,7 @@ class EpicsMCARecord(Device):
     background = C(EpicsSignalRO, '.BG', kind='omitted')
     mode = C(EpicsSignal, '.MODE', string=True, kind='omitted')
 
-    rois = DDC(add_rois(range(0, 32), kind='omitted'),
-               default_read_attrs=RESPECT_KIND,
-               default_configuration_attrs=RESPECT_KIND, kind='omitted')
+    rois = DDC(add_rois(range(0, 32), kind='omitted'), kind='omitted')
 
     def __init__(self, *args, **kwargs):
 

ophyd/scaler.py
@@ -6,7 +6,7 @@ from .ophydobj import Kind
 from .signal import (EpicsSignal, EpicsSignalRO)
 from .device import Device
 from .device import (Component as C, DynamicDeviceComponent as DDC,
-                     FormattedComponent as FC, RESPECT_KIND)
+                     FormattedComponent as FC)
 
 logger = logging.getLogger(__name__)
 
@@ -23,8 +23,6 @@ def _scaler_fields(cls, attr_base, field_base, range_, **kwargs):
 
 class EpicsScaler(Device):
     '''SynApps Scaler Record interface'''
-    _default_configuration_attrs = RESPECT_KIND
-    _default_read_attrs = RESPECT_KIND
     # tigger + trigger mode
     count = C(EpicsSignal, '.CNT', trigger_value=1, kind=Kind.OMITTED)
     count_mode = C(EpicsSignal, '.CONT', string=True, kind=Kind.CONFIG)
@@ -35,13 +33,9 @@ class EpicsScaler(Device):
 
     # the data
     channels = DDC(_scaler_fields(EpicsSignalRO, 'chan', '.S', range(1, 33),
-                                  kind=Kind.HINTED),
-                   default_read_attrs=RESPECT_KIND,
-                   default_configuration_attrs=RESPECT_KIND)
+                                  kind=Kind.HINTED))
     names = DDC(_scaler_fields(EpicsSignal, 'name', '.NM', range(1, 33),
-                               kind=Kind.CONFIG),
-                default_read_attrs=RESPECT_KIND,
-                default_configuration_attrs=RESPECT_KIND)
+                               kind=Kind.CONFIG))
 
     time = C(EpicsSignal, '.T', kind=Kind.CONFIG)
     freq = C(EpicsSignal, '.FREQ', kind=Kind.CONFIG)
@@ -50,13 +44,9 @@ class EpicsScaler(Device):
     auto_count_time = C(EpicsSignal, '.TP1', kind=Kind.CONFIG)
 
     presets = DDC(_scaler_fields(EpicsSignal, 'preset', '.PR', range(1, 33),
-                                 kind=Kind.OMITTED),
-                  default_read_attrs=RESPECT_KIND,
-                  default_configuration_attrs=RESPECT_KIND)
+                                 kind=Kind.OMITTED))
     gates = DDC(_scaler_fields(EpicsSignal, 'gate', '.G', range(1, 33),
-                               kind=Kind.OMITTED),
-                default_read_attrs=RESPECT_KIND,
-                default_configuration_attrs=RESPECT_KIND)
+                               kind=Kind.OMITTED))
 
     update_rate = C(EpicsSignal, '.RATE', kind=Kind.CONFIG)
     auto_count_update_rate = C(EpicsSignal, '.RAT1', kind=Kind.CONFIG)
@@ -71,8 +61,6 @@ class EpicsScaler(Device):
 
 
 class ScalerChannel(Device):
-    _default_configuration_attrs = RESPECT_KIND
-    _default_read_attrs = RESPECT_KIND
 
     # TODO set up monitor on this to automatically change the name
     chname = FC(EpicsSignal, '{self.prefix}.NM{self._ch_num}',
@@ -105,13 +93,9 @@ def _sc_chans(attr_fix, id_range):
 
 
 class ScalerCH(Device):
-    _default_configuration_attrs = RESPECT_KIND
-    _default_read_attrs = RESPECT_KIND
 
     # The data
-    channels = DDC(_sc_chans('chan', range(1, 33)),
-                   default_read_attrs=RESPECT_KIND,
-                   default_configuration_attrs=RESPECT_KIND)
+    channels = DDC(_sc_chans('chan', range(1, 33)))
 
     # tigger + trigger mode
     count = C(EpicsSignal, '.CNT', trigger_value=1, kind=Kind.OMITTED)

ophyd/sim.py
@@ -13,7 +13,7 @@ import uuid
 
 from .signal import Signal
 from .status import DeviceStatus, StatusBase
-from .device import Device, Component, Component as C, Kind, RESPECT_KIND
+from .device import Device, Component, Component as C, Kind
 from types import SimpleNamespace
 from .pseudopos import (PseudoPositioner, PseudoSingle,
                         real_position_argument, pseudo_position_argument)
@@ -747,9 +747,6 @@ class NumpySeqHandler:
 
 
 class ABDetector(Device):
-    _default_read_attrs = RESPECT_KIND
-    _default_configuration_attrs = RESPECT_KIND
-
     a = Component(SynSignal, func=random.random, kind=Kind.HINTED)
     b = Component(SynSignal, func=random.random)
 
@@ -758,17 +755,11 @@ class ABDetector(Device):
 
 
 class DetWithCountTime(Device):
-    _default_read_attrs = RESPECT_KIND
-    _default_configuration_attrs = RESPECT_KIND
-
     intensity = Component(SynSignal, func=lambda: 0, kind=Kind.HINTED)
     count_time = Component(Signal)
 
 
 class DetWithConf(Device):
-    _default_read_attrs = RESPECT_KIND
-    _default_configuration_attrs = RESPECT_KIND
-
     a = Component(SynSignal, func=lambda: 1, kind=Kind.HINTED)
     b = Component(SynSignal, func=lambda: 2, kind=Kind.HINTED)
     c = Component(SynSignal, func=lambda: 3)
@@ -796,9 +787,6 @@ class InvariantSignal(SynSignal):
 
 
 class SPseudo3x3(PseudoPositioner):
-    _default_read_attrs = RESPECT_KIND
-    _default_configuration_attrs = RESPECT_KIND
-
     pseudo1 = C(PseudoSingle, limits=(-10, 10), egu='a', kind=Kind.HINTED)
     pseudo2 = C(PseudoSingle, limits=(-10, 10), egu='b', kind=Kind.HINTED)
     pseudo3 = C(PseudoSingle, limits=None, egu='c', kind=Kind.HINTED)
@@ -826,9 +814,6 @@ class SPseudo3x3(PseudoPositioner):
 
 
 class SPseudo1x3(PseudoPositioner):
-    _default_read_attrs = RESPECT_KIND
-    _default_configuration_attrs = RESPECT_KIND
-
     pseudo1 = C(PseudoSingle, limits=(-10, 10), kind=Kind.HINTED)
     real1 = C(SoftPositioner, init_pos=0)
     real2 = C(SoftPositioner, init_pos=0)

ophyd/tests/test_kind.py
@@ -1,4 +1,4 @@
-from ophyd import Device, Signal, Kind, Component, RESPECT_KIND, kind_context
+from ophyd import Device, Signal, Kind, Component, ALL_COMPONENTS, kind_context
 import pytest
 
 
@@ -40,8 +40,6 @@ def test_standalone_signals():
 def test_nested_devices():
 
     class A(Device):
-        _default_read_attrs = RESPECT_KIND
-        _default_configuration_attrs = RESPECT_KIND
         normal_sig = Component(Signal)
         config_sig = Component(Signal, kind=Kind.CONFIG)
         omitted_sig = Component(Signal, kind=Kind.OMITTED)
@@ -61,8 +59,6 @@ def test_nested_devices():
     # Another layer of nesting!
 
     class B(Device):
-        _default_read_attrs = RESPECT_KIND
-        _default_configuration_attrs = RESPECT_KIND
         a_default = Component(A)
         a_config = Component(A, kind=Kind.CONFIG)
         a_omitted = Component(A, kind=Kind.OMITTED)
@@ -98,8 +94,6 @@ def test_strings():
 
 def test_kind_context():
     class Thing(Device):
-        _default_read_attrs = RESPECT_KIND
-        _default_configuration_attrs = RESPECT_KIND
 
         with kind_context('omitted') as Cpt:
             a = Cpt(Signal)
@@ -297,23 +291,17 @@ def test_back_compat():
 @pytest.fixture(scope='function')
 def thing_haver_haver():
     class Thing(Device):
-        _default_read_attrs = RESPECT_KIND
-        _default_configuration_attrs = RESPECT_KIND
         a = Component(Signal, kind=Kind.OMITTED)
         b = Component(Signal, kind=Kind.CONFIG)
         c = Component(Signal, kind=Kind.NORMAL)
         d = Component(Signal, kind=Kind.HINTED)
 
     class ThingHaver(Device):
-        _default_read_attrs = RESPECT_KIND
-        _default_configuration_attrs = RESPECT_KIND
         A = Component(Thing, kind=Kind.OMITTED)
         B = Component(Thing, kind=Kind.CONFIG)
         C = Component(Thing, kind=Kind.NORMAL)
 
     class ThingHaverHaver(Device):
-        _default_read_attrs = RESPECT_KIND
-        _default_configuration_attrs = RESPECT_KIND
         alpha = Component(ThingHaver, kind=Kind.OMITTED)
         beta = Component(ThingHaver, kind=Kind.CONFIG)
         gamma = Component(ThingHaver, kind=Kind.NORMAL)

commit d08b00eef241e9f97d65b2fe4b3b3c6cf0e48eb7

FIX: Adjust QuadEM to fix bug introduced in cd4d65e3.

ophyd/quadem.py
@@ -20,6 +20,11 @@ def _current_fields(attr_base, field_base, range_, **kwargs):
 
 
 class QuadEM(SingleTrigger, DetectorBase):
+    # These settings intentionally shadow the settings inherited from
+    # DetectorBase.
+    _default_read_attrs = None
+    _default_configuration_attrs = None
+
     _status_type = DeviceStatus  # overrriding the default in SingleTrigger
 
     # This is needed because ophyd verifies that it can see all