c860a185a64289defc2f1ba2268870a5c26149e5
Thomas A Caswell <tcaswell@bnl.gov>
2018-05-15 12:30:14 -0400

    DOC: fix version numbers in deprecation warning

--- ophyd/utils/startup.py
+++ ophyd/utils/startup.py
@@ -8,7 +8,7 @@ __all__ = ['setup', 'logger']
 
 def setup():
     from .. import get_cl
-    warnings.warn("This function is deprecated in ophyd 1.1 "
-                  "and will be removed in ophyd 1.2. "
+    warnings.warn("This function is deprecated in ophyd 1.2 "
+                  "and will be removed in ophyd 1.3. "
                   "setup is now automatically called.")
     return get_cl().setup(logger)
