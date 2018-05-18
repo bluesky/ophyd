283195a691728cd8832e79a9cc0d1bf485435729
Zachary Lentz <zlentz@slac.stanford.edu>
2018-05-18 13:39:39 -0700

    TST: bring in make_fake_device tests from slac

--- ophyd/tests/test_sim.py
+++ ophyd/tests/test_sim.py
@@ -1,5 +1,11 @@
-from ophyd.sim import SynGauss, Syn2DGauss, SynAxis
+from ophyd.sim import (SynGauss, Syn2DGauss, SynAxis,
+                       make_fake_device, FakeEpicsSignal, FakeEpicsSignalRO)
+from ophyd.device import (Device, Component as Cpt, FormattedComponent as FCpt,
+                          DynamicDeviceComponent as DDCpt)
+from ophyd.signal import Signal, EpicsSignal, EpicsSignalRO
+from ophyd.utils import ReadOnlyError, LimitError, DisconnectedError
 import numpy as np
+import pytest
 
 
 def test_random_state_gauss1d():
@@ -106,3 +112,88 @@ def test_synaxis_timestamps():
     motor.setpoint.put(3)
     time.sleep(2*motor.delay)
     orig_time = tester(motor, orig_time)
+
+
+# Classes for testing make_fake_device
+class SampleNested(Device):
+    yolk = Cpt(EpicsSignal, ':YOLK')
+    whites = Cpt(EpicsSignalRO, ':WHITES')
+
+
+class Sample(Device):
+    egg = Cpt(SampleNested, ':EGG')
+    butter = Cpt(EpicsSignal, ':BUTTER')
+    flour = Cpt(EpicsSignalRO, ':FLOUR')
+    sink = FCpt(EpicsSignal, '{self.sink_location}:SINK')
+    fridge = DDCpt({'milk': (EpicsSignal, ':MILK', {}),
+                    'cheese': (EpicsSignalRO, ':CHEESE', {})})
+    nothing = Cpt(Signal)
+
+    sink_location = 'COUNTER'
+
+
+def test_make_fake_device():
+    assert make_fake_device(EpicsSignal) == FakeEpicsSignal
+    assert make_fake_device(EpicsSignalRO) == FakeEpicsSignalRO
+
+    FakeSample = make_fake_device(Sample)
+    my_fake = FakeSample('KITCHEN', name='kitchen')
+    assert isinstance(my_fake, Sample)
+
+    # Skipped
+    assert my_fake.nothing.__class__ is Signal
+
+    # Normal
+    assert isinstance(my_fake.butter, FakeEpicsSignal)
+    assert isinstance(my_fake.flour, FakeEpicsSignalRO)
+    assert isinstance(my_fake.sink, FakeEpicsSignal)
+
+    # Nested
+    assert isinstance(my_fake.egg.yolk, FakeEpicsSignal)
+    assert isinstance(my_fake.egg.whites, FakeEpicsSignalRO)
+
+    # Dynamic
+    assert isinstance(my_fake.fridge.milk, FakeEpicsSignal)
+    assert isinstance(my_fake.fridge.cheese, FakeEpicsSignalRO)
+
+    my_fake.read()
+
+
+def test_do_not_break_real_class():
+    make_fake_device(Sample)
+    assert Sample.butter.cls is EpicsSignal
+    assert Sample.egg.cls is SampleNested
+    assert SampleNested.whites.cls is EpicsSignalRO
+    assert Sample.fridge.defn['milk'][0] is EpicsSignal
+
+    with pytest.raises(DisconnectedError):
+        my_real = Sample('KITCHEN', name='kitchen')
+        my_real.read()
+
+
+def test_fake_epics_signal():
+    sig = FakeEpicsSignal('PVNAME', name='sig', limits=True)
+    sig.sim_limits((0, 10))
+    with pytest.raises(LimitError):
+        sig.put(11)
+    sig.put(4)
+    assert sig.get() == 4
+    sig.sim_put(5)
+    assert sig.get() == 5
+    sig.sim_set_putter(lambda x: sig.sim_put(x + 1))
+    sig.put(6)
+    assert sig.get() == 7
+    sig.sim_set_getter(lambda: 2.3)
+    assert sig.get() == 2.3
+
+
+def test_fake_epics_signal_ro():
+    sig = FakeEpicsSignalRO('PVNAME', name='sig')
+    with pytest.raises(ReadOnlyError):
+        sig.put(3)
+    with pytest.raises(ReadOnlyError):
+        sig.put(4)
+    with pytest.raises(ReadOnlyError):
+        sig.set(5)
+    sig.sim_put(1)
+    assert sig.get() == 1
