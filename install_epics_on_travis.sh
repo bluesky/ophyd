de37e9d0344255382c27ecb983fb607f546535ee
K Lauer <klauer@slac.stanford.edu>
2018-11-13 11:35:24 -0800

    CI: epics-on-travis, take 3

--- install_epics_on_travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-sudo apt-get update
-sudo apt-get install -yq wget
-wget --quiet http://epics.nsls2.bnl.gov/debian/repo-key.pub -O - | apt-key add -
-echo "deb http://epics.nsls2.bnl.gov/debian/ $EPICS_DEB_DIST main contrib" | tee /etc/apt/sources.list.d/nsls2.list
-sudo apt-get update
-sudo apt-get install -yq epics-dev
---
+++ ophyd/tests/fake_motor_ioc.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from caproto.server import pvproperty, PVGroup, ioc_arg_parser, run
+import numpy as np
+
+
+class FakeMotorIOC(PVGroup):
+    setpoint = pvproperty(value=0.0, precision=1)
+    readback = pvproperty(value=0.0, read_only=True, precision=1)
+    moving = pvproperty(value=0.0, read_only=True)
+    actuate = pvproperty(value=0)
+    stop = pvproperty(value=0)
+    step_size = pvproperty(value=0.1)
+
+    @actuate.scan(period=0.1)
+    async def actuate(self, instance, async_lib):
+        step_size = self.step_size.value
+        setpoint = self.setpoint.value
+        readback = self.readback.value
+        moving = self.moving.value
+        actuate = self.actuate.value
+        stop = self.stop.value
+
+        if stop:
+            await self.stop.write(0)
+            await self.moving.write(0)
+        elif actuate or moving:
+            if moving != 1:
+                await self.actuate.write(0)
+                await self.moving.write(1)
+
+            delta = setpoint - readback
+            if abs(delta) <= step_size:
+                await self.readback.write(setpoint)
+                await self.moving.write(0)
+            else:
+                await self.readback.write(readback + np.sign(delta) * step_size)
+
+
+if __name__ == '__main__':
+    ioc_options, run_options = ioc_arg_parser(
+        default_prefix='fake_motor:',
+        desc="An IOC which mocks a simple motor")
+    ioc = FakeMotorIOC(**ioc_options)
+    run(ioc.pvdb, **run_options)
