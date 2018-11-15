0171df5960af28ef7f793d7d635ffdc94f8f04d5
K Lauer <kenneth.lauer@gmail.com>
2018-11-15 10:00:18 -0800

    TST: move quadem to make_fake_device

--- ophyd/tests/test_quadem.py
+++ ophyd/tests/test_quadem.py
@@ -1,67 +1,36 @@
 import logging
 import pytest
 
-from ophyd import QuadEM, Component as Cpt, Signal
+from ophyd import QuadEM, Kind
+from ophyd.sim import (make_fake_device, clear_fake_device)
 from ophyd.areadetector.plugins import ImagePlugin, StatsPlugin
-from ophyd.ophydobj import Kind
-from .conftest import using_fake_epics_pv
 
 
 logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope='function')
-@using_fake_epics_pv
 def quadem():
-    class FakeStats(StatsPlugin):
-        plugin_type = Cpt(Signal, value=StatsPlugin._plugin_type)
-        nd_array_port = Cpt(Signal, value='NSLS_EM')
-
-    class FakeImage(ImagePlugin):
-        plugin_type = Cpt(Signal, value=ImagePlugin._plugin_type)
-        nd_array_port = Cpt(Signal, value='NSLS_EM')
-
-    class FakeQuadEM(QuadEM):
-        image = Cpt(FakeImage, 'image1:')
-        current1 = Cpt(FakeStats, 'Current1:')
-        current2 = Cpt(FakeStats, 'Current2:')
-        current3 = Cpt(FakeStats, 'Current3:')
-        current4 = Cpt(FakeStats, 'Current4:')
-        sum_all = Cpt(FakeStats, 'SumAll:')
-
+    FakeQuadEM = make_fake_device(QuadEM)
     em = FakeQuadEM('quadem:', name='quadem')
+    clear_fake_device(em)
 
-    ''' Beware: Ugly Hack below
+    em.conf.port_name.put('NSLS_EM')
 
-        tl;dr
-        Set Signal._read_pv = Signal._write_pv in order for
-        set_and_wait() to succeed in calls from Device.stage()
-
-        The raison d'etre here is a limitation of FakeEpicsPV,
-        or rather a limitation of the test harness:
-            Since the QuadEM is based on areadetector, it uses
-            EpicsSignalWithRBV in several places. The test harness
-            monkey-patches epics.PV with FakeEpicsPV, which means
-            that get/put are routed to two different pvs, which in turn,
-            means that set_and_wait() will never be successful for
-            EpicsSignalWithRBVs... :-(
-    '''
-
-    for sig in em.stage_sigs:
-        sig = getattr(em, sig)
-        sig._read_pv = sig._write_pv
-
-    for k in ['image', *['current{}'.format(j) for j in range(1, 5)],
+    for k in ['image', 'current1', 'current2', 'current3', 'current4',
               'sum_all']:
         cc = getattr(em, k)
-        for sig in cc.stage_sigs:
-            sig = getattr(cc, sig)
-            sig._read_pv = sig._write_pv
-        cc.enable._read_pv = cc.enable._write_pv
-        cc.enable._write_pv.enum_strs = ['Disabled', 'Enabled']
+
+        if isinstance(cc, ImagePlugin):
+            cc.plugin_type.sim_put(ImagePlugin._plugin_type)
+            cc.nd_array_port.sim_put('NSLS_EM')
+        elif isinstance(cc, StatsPlugin):
+            cc.plugin_type.sim_put(StatsPlugin._plugin_type)
+            cc.nd_array_port.sim_put('NSLS_EM')
+
+        cc.enable.sim_set_enum_strs(['Disabled', 'Enabled'])
         cc.enable.put('Enabled')
-        cc.port_name._read_pv.put(k.upper())
-    ''' End: Ugly Hack '''
+        cc.port_name.sim_put(k.upper())
 
     em.wait_for_connection()
 
@@ -72,7 +41,6 @@ def test_connected(quadem):
     assert quadem.connected
 
 
-@using_fake_epics_pv
 def test_scan_point(quadem):
     assert quadem._staged.value == 'no'
 
@@ -84,15 +52,8 @@ def test_scan_point(quadem):
     assert quadem._staged.value == 'no'
 
 
-@using_fake_epics_pv
 def test_reading(quadem):
     assert 'current1.mean_value' in quadem.read_attrs
-
-    desc = quadem.describe()
-    desc_keys = list(desc['quadem_current1_mean_value'].keys())
-    assert (set(desc_keys) == set(['dtype', 'precision', 'shape', 'source',
-                                   'units', 'lower_ctrl_limit',
-                                   'upper_ctrl_limit']))
 
     vals = quadem.read()
     assert 'quadem_current1_mean_value' in vals
@@ -108,7 +69,6 @@ def test_reading(quadem):
     assert rc.keys() == dc.keys()
 
 
-@using_fake_epics_pv
 def test_hints(quadem):
 
     desc = quadem.describe()
