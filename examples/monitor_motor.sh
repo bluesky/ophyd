7834832a703c8a2a3eafcf28995ff95146b25f20
K Lauer <klauer@slac.stanford.edu>
2018-11-27 10:53:03 -0800

    CLN: remove outdated examples, scratch files, unused scripts

--- examples/monitor_motor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Monitor the motor status as you run the example scripts
-
-camonitor XF:31IDA-OP{Tbl-Ax:X1}Mtr.VAL XF:31IDA-OP{Tbl-Ax:X1}Mtr.RBV XF:31IDA-OP{Tbl-Ax:FakeMtr}-I  XF:31IDA-OP{Tbl-Ax:X1}Mtr.VAL  XF:31IDA-OP{Tbl-Ax:FakeMtr}-SP
