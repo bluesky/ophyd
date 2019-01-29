03028eddd2f495c2ed5311ba707857ff924a5931
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2019-01-29 19:49:05 +0100

    Test the impact of an missing readback

---
+++ ophyd/tests/test_missing_readback.py
@@ -0,0 +1,33 @@
+"""Check if a missing value stops program
+"""
+import unittest
+
+from ophyd import sim, Device, Component as Cpt
+
+
+class TestDevice(Device):
+    """Just to create a device
+    """
+    signal = Cpt(sim.FakeEpicsSignalRO, 'a_fake_signal')
+
+
+class TestMissingReadBack(unittest.TestCase):
+
+    def setUp(self):
+        self.fake_signal = TestDevice(name = "fake_device")
+
+    def testDescribeFail(self):
+        """If readback was never set does descirbe fail?
+
+        I guess that's a bug
+        """
+        self.assertRaises(ValueError, self.fake_signal.describe)
+
+    def testDescribeAfterSet(self):
+        """If the value is once set it does not fail any longer
+        """
+        fs = self.fake_signal
+        fs.signal.sim_put(42)
+
+if __name__ == '__main__':
+    unittest.main()
