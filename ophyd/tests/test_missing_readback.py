2e1b0ea783ff3acc573b3da5b4c17ae1a5f22268
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2019-01-30 11:10:20 +0100

    Test converted to pytest
    Signed-off-by: Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>

--- ophyd/tests/test_missing_readback.py
+++ ophyd/tests/test_missing_readback.py
@@ -1,35 +1,32 @@
 """Check if a missing value stops program
 """
-import unittest
-
+import pytest
 from ophyd import sim, Device, Component as Cpt
 
-
-class TestDevice(Device):
+class ADevice(Device):
     """Just to create a device
     """
     signal = Cpt(sim.FakeEpicsSignalRO, 'a_fake_signal')
 
 
-class TestMissingReadBack(unittest.TestCase):
+def _get_fake_signal():
+    return ADevice(name = "fake_device")
 
-    def setUp(self):
-        self.fake_signal = TestDevice(name = "fake_device")
+def test_describe_fail():
+    """If readback was never set, does descirbe fail?
 
-    def testDescribeFail(self):
-        """If readback was never set does descirbe fail?
+    I guess that's a bug
+    """
+    # self.assertRaises(ValueError, self.fake_signal.describe)
+    fs = _get_fake_signal()
+    with pytest.raises(AssertionError):
+        fs.describe()
 
-        I guess that's a bug
-        """
-        # self.assertRaises(ValueError, self.fake_signal.describe)
-        self.assertRaises(AssertionError, self.fake_signal.describe)
+def test_describe_after_set():
+    """If readback was set does descirbe fail?
 
-    def testDescribeAfterSet(self):
-        """If the value is once set it does not fail any longer
-        """
-        fs = self.fake_signal
-        fs.signal.sim_put(42)
-
-if __name__ == '__main__':
-    #TestDevice(name = "fake_device").describe()
-    unittest.main()
+    SHould work
+    """
+    fs = _get_fake_signal()
+    fs.signal.sim_put(42)
+    fs.describe()
