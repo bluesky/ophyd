c98c4be67f1c0e95e3072e4c70546b0455699fa2
Joshua Lynch <jlynch@bnl.gov>
2019-11-08 17:14:45 -0500

    pin caproto requirement and allow setup.py to install test dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 import os
 from setuptools import (setup, find_packages)
+import sys
 import versioneer
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the requirements from requirements.txt
 with open(os.path.join(here, 'requirements.txt'), 'rt') as f:
-    requirements = f.read().split()
+    requirements = [r.strip() for r in f.readlines() if not r.startswith('git+')]
+
+with open('test-requirements.txt') as f:
+    test_requirements = [r.strip() for r in f.readlines()]
+
+if sys.version_info < (3, 6):
+    caproto_warning = """
+Python 3.6 or above is required for caproto.
+The caproto tests will not be run.
+"""
+    print('*' * 45)
+    print(caproto_warning)
+    print('*' * 45)
+
+    # remove caproto from test_requirements
+    test_requirements = [r for r in test_requirements if not r.startswith('caproto')]
+
+extras_require = {
+    'test': test_requirements,
+}
 
 # Get the long description from the README file
 with open(os.path.join(here, 'README.md'), 'rt', encoding='utf-8') as f:
@@ -21,6 +41,7 @@ setup(name='ophyd',
       license='BSD',
       install_requires=requirements,
       packages=find_packages(),
+      extras_require=extras_require,
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Programming Language :: Python :: 3.6",
