bd433bb67e810a48ca1bc196850e8961c0b0cb73
Thomas A Caswell <tcaswell@bnl.gov>
2020-03-12 22:35:50 -0400

    TST: use .get() or .value as needed by the monitor/unmonitor-ness

--- ophyd/tests/test_timestamps.py
+++ ophyd/tests/test_timestamps.py
@@ -15,17 +15,17 @@ def test_read_pv_timestamp_no_monitor(motor):
     sp = EpicsSignal(motor.user_setpoint.pvname, name='test')
     rbv = EpicsSignalRO(motor.user_readback.pvname, name='test')
 
+    assert rbv.get() == sp.get()
     rbv_value0 = rbv.get()
     ts0 = rbv.timestamp
     sp.put(sp.get() + 0.1)
-    time.sleep(0.1)
-
+    time.sleep(.5)
     rbv_value1 = rbv.get()
     ts1 = rbv.timestamp
     assert ts1 > ts0
     assert_almost_equal(rbv_value0 + 0.1, rbv_value1)
 
-    sp.put(sp.value - 0.1, wait=True)
+    sp.put(sp.get() - 0.1)
 
 
 @pytest.mark.motorsim
@@ -43,7 +43,7 @@ def test_write_pv_timestamp_no_monitor(motor):
     assert ts1 > ts0
     assert_almost_equal(sp_value0 + 0.1, sp_value1)
 
-    sp.put(sp.value - 0.1, wait=True)
+    sp.put(sp.get() - 0.1, wait=True)
 
 
 @pytest.mark.motorsim
@@ -55,12 +55,12 @@ def test_read_pv_timestamp_monitor(motor):
     rbv = EpicsSignalRO(motor.user_readback.pvname, auto_monitor=True,
                         name='test')
 
-    rbv_value0 = rbv.get()
+    rbv_value0 = rbv.value
     ts0 = rbv.timestamp
     sp.put(rbv_value0 + 0.1, wait=True)
     time.sleep(0.2)
 
-    rbv_value1 = rbv.get()
+    rbv_value1 = rbv.value
     ts1 = rbv.timestamp
     assert ts1 > ts0
     assert_almost_equal(rbv_value0 + 0.1, rbv_value1)
@@ -74,12 +74,12 @@ def test_write_pv_timestamp_monitor(motor):
     sp = EpicsSignal(motor.user_setpoint.pvname, auto_monitor=True,
                      name='test')
 
-    sp_value0 = sp.get()
+    sp_value0 = sp.value
     ts0 = sp.timestamp
     sp.put(sp_value0 + 0.1, wait=True)
     time.sleep(0.1)
 
-    sp_value1 = sp.get()
+    sp_value1 = sp.value
     ts1 = sp.timestamp
     assert ts1 > ts0
     assert_almost_equal(sp_value0 + 0.1, sp_value1)
