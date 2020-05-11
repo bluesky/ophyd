77d2721ea6f945d1d17bc73b519b62d71c85e56f
Thomas A Caswell <tcaswell@bnl.gov>
2020-05-11 19:33:44 -0400

    STY: the error code reported for non-top of module imports changed
    Seems to now be E402 instead of F402 (?).

--- ophyd/areadetector/__init__.py
+++ ophyd/areadetector/__init__.py
@@ -5,18 +5,18 @@ import logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-from .base import *  # noqa: F401, F402, F403
-from .cam import *  # noqa: F401, F402, F403
-from .detectors import *  # noqa: F401, F402, F403
+from .base import *  # noqa: F401, F402, E402, F403
+from .cam import *  # noqa: F401, F402, E402, F403
+from .detectors import *  # noqa: F401, F402, E402, F403
 
 # NOTE: the following imports are here for backward compatibility with
 # previous ophyd versions. This does not represent all available plugins
 # in ophyd. For that, import directly from ophyd.areadetector.plugins.
-from .plugins import (ColorConvPlugin, FilePlugin, HDF5Plugin, ImagePlugin,  # noqa: F401, F402
+from .plugins import (ColorConvPlugin, FilePlugin, HDF5Plugin, ImagePlugin,  # noqa: F401, F402, E402
                       JPEGPlugin, MagickPlugin, NetCDFPlugin, NexusPlugin,  # noqa: F401
                       OverlayPlugin, ProcessPlugin, ROIPlugin, StatsPlugin,
                       TIFFPlugin, TransformPlugin, get_areadetector_plugin,
                       plugin_from_pvname, register_plugin)
 
-from .common_plugins import *  # noqa: F401, F402, F403
-from .trigger_mixins import *  # noqa: F401, F402, F403
+from .common_plugins import *  # noqa: F401, F402, E402, F403
+from .trigger_mixins import *  # noqa: F401, F402, E402, F403
