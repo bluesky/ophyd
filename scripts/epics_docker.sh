2912972a6eb84203562eb9d05ba571d1909c3fbb
Dan Allan <dallan@bnl.gov>
2020-11-10 13:47:28 -0500

    Pull from nsls2 docker account (not klauer).

--- scripts/epics_docker.sh
+++ scripts/epics_docker.sh
@@ -9,16 +9,15 @@ fi
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SCRIPTS_DIR/epics_exports.sh
 
-DOCKERIMAGE="klauer/epics-docker"
-PE_DOCKERIMAGE="klauer/simioc-docker"
-PE_DOCKERTAG="pyepics-docker"
+DOCKERIMAGE="nsls2/epics-docker:latest"
+PE_DOCKERIMAGE="nsls2/pyepics-docker:latest"
 
 docker pull ${DOCKERIMAGE}
-docker pull ${PE_DOCKERIMAGE}:${PE_DOCKERTAG}
+docker pull ${PE_DOCKERIMAGE}
 mkdir -p /tmp/data
 # this is required because the images use a version of AD which
 # does not create missing directories.
 python -c "import ophyd.utils.paths as oup; import datetime; now = datetime.datetime.now(); [oup.make_dir_tree(now.year + j, base_path='/tmp/data') for j in [-1, 0, 1]]"
 docker run -d -p $DOCKER0_IP:7000-9000:5064/tcp -v /tmp/data:/data ${DOCKERIMAGE}
-docker run -d -p $DOCKER0_IP:7000-9000:5064/tcp ${PE_DOCKERIMAGE}:${PE_DOCKERTAG}
+docker run -d -p $DOCKER0_IP:7000-9000:5064/tcp ${PE_DOCKERIMAGE}
 
