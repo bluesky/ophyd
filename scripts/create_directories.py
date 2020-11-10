f39b42b2794dee9242114e67122f1ac2bc5ccaf6
Dan Allan <dallan@bnl.gov>
2020-11-10 14:11:12 -0500

    Add comments.

--- scripts/create_directories.py
+++ scripts/create_directories.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"Create directories where Area Detector will save its data during tests."
 from ophyd.utils.paths import make_dir_tree
 import datetime
 
@@ -6,6 +7,7 @@ import datetime
 def main(args):
     base_path = args[1]  # e.g. "/tmp/data"
     now = datetime.datetime.now()
+    # Make YYYY/MM/DD/ directories for yesterday, today, and tomorrow.
     for offset in (-1, 0, 1):
         make_dir_tree(now.year + j, base_path=base_path)
 
