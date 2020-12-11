065ebfac2a5115a1e6125ca2e455e05f85c04d5b
Dan Allan <dallan@bnl.gov>
2020-12-11 14:14:16 -0500

    Only install test, docs dependencies where needed.

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -9,7 +9,5 @@ python -m pip install --upgrade pip setuptools wheel numpy
 # Verioneer uses the most recent git tag to generate __version__, which appears
 # in the published documentation.
 git fetch --tags
-pip install .
-pip install -r requirements-test.txt
-pip install -r requirements-docs.txt
-pip list
+python -m pip install .
+python -m pip list
