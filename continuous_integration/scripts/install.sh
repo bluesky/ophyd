37b45d78cc1b01a93e6f8b14117b35bb38a326fe
Dan Allan <dallan@bnl.gov>
2020-12-11 11:50:53 -0500

    Install 'wheel'.

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -5,8 +5,8 @@ sudo apt-get install graphviz procserv libhdf5-dev
 # These packages are installed in the base environment but may be older
 # versions. Explicitly upgrade them because they often create
 # installation problems if out of date.
-python -m pip install --upgrade pip setuptools numpy
-# Verioneer uses the most recent git tag to generate __version__, which appers
+python -m pip install --upgrade pip setuptools wheel numpy
+# Verioneer uses the most recent git tag to generate __version__, which appears
 # in the published documentation.
 git fetch --tags
 pip install .
