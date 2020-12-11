a76f058fa529676bef4bccb0a14178250112a06f
Dan Allan <dallan@bnl.gov>
2020-12-11 09:59:21 -0500

    Fetch git tags during installation.

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -6,6 +6,9 @@ sudo apt install graphviz procserv libhdf5-dev
 # versions. Explicitly upgrade them because they often create
 # installation problems if out of date.
 python -m pip install --upgrade pip setuptools numpy
+# Verioneer uses the most recent git tag to generate __version__, which appers
+# in the published documentation.
+git fetch --tags
 pip install .
 pip install -r requirements-docs.txt
 pip list
