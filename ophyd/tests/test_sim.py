3d442513e4814c8a0a582823196983734402ca4b
Dominic Oram <dominic.oram@diamond.ac.uk>
2022-05-06 10:34:17 +0100

    ENH: Added FakeEpicsPathSignal for testing

--- ophyd/tests/test_sim.py
+++ ophyd/tests/test_sim.py
@@ -1,11 +1,13 @@
 from ophyd.sim import (SynGauss, Syn2DGauss, SynAxis, make_fake_device,
                        FakeEpicsSignal, FakeEpicsSignalRO,
-                       FakeEpicsSignalWithRBV, clear_fake_device,
-                       instantiate_fake_device, SynSignalWithRegistry)
+                       FakeEpicsSignalWithRBV, FakeEpicsPathSignal,
+                       clear_fake_device, instantiate_fake_device,
+                       SynSignalWithRegistry)
 from ophyd.device import (Device, Component as Cpt, FormattedComponent as FCpt,
                           DynamicDeviceComponent as DDCpt)
 from ophyd.signal import Signal, EpicsSignal, EpicsSignalRO
 from ophyd.areadetector.base import EpicsSignalWithRBV
+from ophyd.areadetector.paths import EpicsPathSignal
 from ophyd.utils import ReadOnlyError, LimitError, DisconnectedError
 import numpy as np
 import pytest
@@ -146,6 +148,7 @@ def test_make_fake_device():
     assert make_fake_device(EpicsSignal) == FakeEpicsSignal
     assert make_fake_device(EpicsSignalRO) == FakeEpicsSignalRO
     assert make_fake_device(EpicsSignalWithRBV) == FakeEpicsSignalWithRBV
+    assert make_fake_device(EpicsPathSignal) == FakeEpicsPathSignal
 
     FakeSample = make_fake_device(Sample)
     my_fake = FakeSample('KITCHEN', name='kitchen')
