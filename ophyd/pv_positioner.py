e07ec912b3a3cf8deeb6b63c40a3ba1d26622809
Zachary Lentz <zlentz@slac.stanford.edu>
2022-05-12 11:10:14 -0700

    DOC: add init parameter docstrings to the pv_positioner utility classes

--- ophyd/pv_positioner.py
+++ ophyd/pv_positioner.py
@@ -317,6 +317,43 @@ class PVPositionerComparator(PVPositioner):
 
     Internally, this will subscribe to both the `setpoint` and `readback`
     signals, updating `done` as appropriate.
+
+    Parameters
+    ----------
+    prefix : str, optional
+        The device prefix used for all sub-positioners. This is optional as it
+        may be desirable to specify full PV names for PVPositioners.
+    limits : 2-element sequence, optional
+        (low_limit, high_limit)
+    name : str
+        The device name
+    egu : str, optional
+        The engineering units (EGU) for the position
+    settle_time : float, optional
+        The amount of time to wait after moves to report status completion
+    timeout : float, optional
+        The default timeout to use for motion requests, in seconds.
+
+    Attributes
+    ----------
+    setpoint : Signal
+        The setpoint (request) signal
+    readback : Signal or None
+        The readback PV (e.g., encoder position PV)
+    actuate : Signal or None
+        The actuation PV to set when movement is requested
+    actuate_value : any, optional
+        The actuation value, sent to the actuate signal when motion is
+        requested
+    stop_signal : Signal or None
+        The stop PV to set when motion should be stopped
+    stop_value : any, optional
+        The value sent to stop_signal when a stop is requested
+    put_complete : bool, optional
+        If set, the specified PV should allow for asynchronous put completion
+        to indicate motion has finished.  If `actuate` is specified, it will be
+        used for put completion.  Otherwise, the `setpoint` will be used.  See
+        the `-c` option from `caput` for more information.
     """
 
     done = Cpt(InternalSignal, value=0)
@@ -390,6 +427,55 @@ class PVPositionerIsClose(PVPositionerComparator):
     able to deviate from the goal position by up to 10% of its value. This
     is useful for small quantities. For example, defining an atol for a
     positioner that ranges from 1e-8 to 2e-8 could be somewhat awkward.
+
+    Parameters
+    ----------
+    prefix : str, optional
+        The device prefix used for all sub-positioners. This is optional as it
+        may be desirable to specify full PV names for PVPositioners.
+    limits : 2-element sequence, optional
+        (low_limit, high_limit)
+    name : str
+        The device name
+    egu : str, optional
+        The engineering units (EGU) for the position
+    settle_time : float, optional
+        The amount of time to wait after moves to report status completion
+    timeout : float, optional
+        The default timeout to use for motion requests, in seconds.
+    atol : float, optional
+        A measure of absolute tolerance. If atol is 0.1, then you'd be
+        able to be up to 0.1 units away and still count as done.
+    rtol : float, optional
+        A measure of relative tolerance. If rtol is 0.1, then you'd be
+        able to deviate from the goal position by up to 10% of its value
+
+    Attributes
+    ----------
+    setpoint : Signal
+        The setpoint (request) signal
+    readback : Signal or None
+        The readback PV (e.g., encoder position PV)
+    actuate : Signal or None
+        The actuation PV to set when movement is requested
+    actuate_value : any, optional
+        The actuation value, sent to the actuate signal when motion is
+        requested
+    stop_signal : Signal or None
+        The stop PV to set when motion should be stopped
+    stop_value : any, optional
+        The value sent to stop_signal when a stop is requested
+    put_complete : bool, optional
+        If set, the specified PV should allow for asynchronous put completion
+        to indicate motion has finished.  If `actuate` is specified, it will be
+        used for put completion.  Otherwise, the `setpoint` will be used.  See
+        the `-c` option from `caput` for more information.
+    atol : float, optional
+        A measure of absolute tolerance. If atol is 0.1, then you'd be
+        able to be up to 0.1 units away and still count as done.
+    rtol : float, optional
+        A measure of relative tolerance. If rtol is 0.1, then you'd be
+        able to deviate from the goal position by up to 10% of its value
     """
 
     atol: Optional[float] = None
@@ -439,6 +525,39 @@ class PVPositionerDone(PVPositioner):
 
     When the user asks for a move, set done to 0 and then back to 1.
     This simulates the normal positioner behavior.
+
+    Parameters
+    ----------
+    prefix : str, optional
+        The device prefix used for all sub-positioners. This is optional as it
+        may be desirable to specify full PV names for PVPositioners.
+    limits : 2-element sequence, optional
+        (low_limit, high_limit)
+    name : str
+        The device name
+    egu : str, optional
+        The engineering units (EGU) for the position
+    settle_time : float, optional
+        The amount of time to wait after moves to report status completion
+    timeout : float, optional
+        The default timeout to use for motion requests, in seconds.
+
+    Attributes
+    ----------
+    actuate : Signal or None
+        The actuation PV to set when movement is requested
+    actuate_value : any, optional
+        The actuation value, sent to the actuate signal when motion is
+        requested
+    stop_signal : Signal or None
+        The stop PV to set when motion should be stopped
+    stop_value : any, optional
+        The value sent to stop_signal when a stop is requested
+    put_complete : bool, optional
+        If set, the specified PV should allow for asynchronous put completion
+        to indicate motion has finished.  If `actuate` is specified, it will be
+        used for put completion.  Otherwise, the `setpoint` will be used.  See
+        the `-c` option from `caput` for more information.
     """
     setpoint = Cpt(EpicsSignal, '', kind='hinted')
 
