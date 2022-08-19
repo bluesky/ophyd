4ec7d8da22fc00801a270032e178c287020ccf51
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-08-19 12:34:22 +0000

    Move settings to setup.cfg and use setuptools-scm
    Move config from .flake8, pytest.ini, setup.py, coveragerc, and
    requirements*.txt to setup.cfg and pyproject.tom. Adopt setuptools-scm
    instead of versioneer (which will also allow us to delete MANIFEST.in).

--- ophyd/__init__.py
+++ ophyd/__init__.py
@@ -78,8 +78,6 @@ def get_cl():
 
 set_cl()
 
-from ._version import get_versions  # noqa: F402, E402
-
 # Areadetector-related
 from .areadetector import *  # noqa: F401, F402, E402, F403
 from .device import (  # noqa: F401, F402, E402
@@ -127,5 +125,13 @@ from .signal import (  # noqa: F401, F402, E402
 from .status import StatusBase, wait  # noqa: F401, F402, E402
 from .utils.startup import setup as setup_ophyd  # noqa: F401, F402, E402
 
-__version__ = get_versions()["version"]
-del get_versions
+try:
+    # Use live version from git
+    from setuptools_scm import get_version
+
+    # Warning: If the install is nested to the same depth, this will always succeed
+    __version__ = get_version(root="..", relative_to=__file__)
+    del get_version
+except (ImportError, LookupError):
+    # Use installed version
+    from ._version import __version__  # noqa: F401
--- setup.py
+++ setup.py
@@ -1,66 +1,7 @@
 #!/usr/bin/env python
-import os
-import sys
-
-from setuptools import find_packages, setup
-
-import versioneer
+from setuptools import setup
 
 # NOTE: This file must remain Python 2 compatible for the foreseeable future,
 # to ensure that we error out properly for people with outdated setuptools
 # and/or pip.
-min_version = (3, 8)
-if sys.version_info < min_version:
-    error = """
-ophyd does not support Python {0}.{1}.
-Python {2}.{3} and above is required. Check your Python version like so:
-
-python3 --version
-
-This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
-Upgrade pip like so:
-
-pip install --upgrade pip
-""".format(
-        *(sys.version_info[:2] + min_version)
-    )
-    sys.exit(error)
-
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Get the requirements from requirements.txt
-with open(os.path.join(here, "requirements.txt"), "rt") as f:
-    requirements = f.read().split()
-
-# Get the long description from the README file
-with open(os.path.join(here, "README.rst"), "rt", encoding="utf-8") as f:
-    long_description = f.read()
-
-setup(
-    name="ophyd",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    long_description=long_description,
-    license="BSD",
-    python_requires=">={}".format(".".join(str(n) for n in min_version)),
-    install_requires=requirements,
-    packages=find_packages(),
-    entry_points={
-        "databroker.handlers": [
-            "NPY_SEQ = ophyd.sim:NumpySeqHandler",
-        ]
-    },
-    include_package_data=True,
-    package_data={
-        # NOTE: this is required in addition to MANIFEST.in, as that only
-        # applies to source distributions
-        # Include our documentation helpers:
-        "": ["*.rst"],
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-)
+setup()
