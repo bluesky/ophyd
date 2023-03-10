50ab578165c488eecfe3c26060883b2a0e956854
tizayi <tizayi0@gmail.com>
2023-03-10 15:02:16 +0000

    BUG: isinstance calls success property before status object is done

--- ophyd/v2/core.py
+++ ophyd/v2/core.py
@@ -81,7 +81,9 @@ class AsyncStatus(Status):
 
     @property
     def success(self) -> bool:
-        assert self.done, "Status has not completed yet"
+        if not self.done:
+            return False
+
         try:
             self.task.result()
         except (Exception, asyncio.CancelledError):
