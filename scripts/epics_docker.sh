d27a577e7db46ae52f4fee9fd1231a7264892149
Thomas A Caswell <tcaswell@bnl.gov>
2024-11-21 13:56:21 -0500

    CI: do not try to start docker images that no longer exist
    For reasons not yet understood, these images now have 0 size on dockerhub

--- scripts/epics_docker.sh
+++ scripts/epics_docker.sh
@@ -11,12 +11,8 @@ sudo cp $SCRIPTS_DIR/docker.conf $file
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-MOTOR_DOCKERIMAGE="nsls2/epics-docker:latest"
-PE_DOCKERIMAGE="nsls2/pyepics-docker:latest"
 AD_DOCKERIMAGE="prjemian/synapps-6.1-ad-3.7:latest"
 
-docker pull ${MOTOR_DOCKERIMAGE}
-docker pull ${PE_DOCKERIMAGE}
 docker pull ${AD_DOCKERIMAGE}
 
 mkdir -p /tmp/ophyd_AD_test/
@@ -26,9 +22,6 @@ mkdir -p /tmp/ophyd_AD_test/
 # does not create missing directories.
 python $SCRIPTS_DIR/create_directories.py /tmp/ophyd_AD_test/data1
 
-docker run --rm -d -v /tmp/ophyd_AD_test:/tmp/ophyd_AD_test/ ${MOTOR_DOCKERIMAGE}
 docker run --name=area-detector --rm -dit -v /tmp/ophyd_AD_test:/tmp/ophyd_AD_test/ -e AD_PREFIX="ADSIM:" ${AD_DOCKERIMAGE} /bin/bash
 sleep 1  # Probably not needed?
 docker exec area-detector iocSimDetector/simDetector.sh start
-docker run --rm -d ${PE_DOCKERIMAGE}
-
