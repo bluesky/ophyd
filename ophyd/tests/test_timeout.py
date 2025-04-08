fb4fea790cc7c1a3fc3c734369d68415544da08e
thomashopkins32 <thopkins1@bnl.gov>
2025-04-08 14:01:49 -0400

    Progress on unit test

--- ophyd/tests/test_timeout.py
+++ ophyd/tests/test_timeout.py
@@ -3,6 +3,7 @@ import logging
 import pytest
 
 from ophyd import Component, Device, EpicsSignal
+from ophyd.signal import EpicsSignalBase
 
 logger = logging.getLogger(__name__)
 
@@ -34,3 +35,20 @@ def test_timeout():
     assert "prefix:_4" in ex_msg
     assert "prefix:_sub_5" in ex_msg
     assert "prefix:3" in ex_msg
+
+
+def test_epics_signal_base_connection_timeout():
+    EpicsSignalBase.set_defaults(connection_timeout=1e-6)
+    class MyDevice(Device):
+        # Should timeout using default connection timeout
+        cpt1 = Component(EpicsSignal, "1")
+        # Should *not* timeout using custom connection timeout
+        cpt2 = Component(EpicsSignal, "2", connection_timeout=1.0)
+
+    device = MyDevice("prefix:", name="dev")
+    with pytest.raises(TimeoutError) as cm:
+        device.cpt1.kind = "hinted"
+    device.cpt2.kind = "hinted"
+
+    ex_msg = str(cm.value)
+    print(ex_msg)
