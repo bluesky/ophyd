7af30ec83123b2aba4228e8f80be4b8492701cf5
Thomas Hopkins <thopkins1@bnl.gov>
2026-06-04 15:53:02 -0400

    dtype_numpy property on DetectorBase, Fix Pilatus to int32 dtype (#1282)
    * Small refactor for easier override of numpy data type; Fix Pilatus to int32
    
    * Update ophyd/areadetector/detectors.py
    
    Co-authored-by: Thomas A Caswell <tcaswell@gmail.com>
    
    * Use Optional typing for Python <3.10
    
    ---------
    
    Co-authored-by: Thomas A Caswell <tcaswell@gmail.com>

--- ophyd/areadetector/detectors.py
+++ ophyd/areadetector/detectors.py
@@ -6,6 +6,7 @@
 .. _areaDetector: https://areadetector.github.io/master/index.html
 """
 import warnings
+from typing import Optional
 
 import numpy as np
 
@@ -102,6 +103,16 @@ class DetectorBase(ADBase):
 
     dispatch.__doc__ = generate_datum.__doc__
 
+    @property
+    def dtype_numpy(self) -> Optional[str]:
+        """The data type of the image in numpy style"""
+        # If the data type isn't disabled, we assume it is accurate
+        return (
+            np.dtype(self.cam.data_type.get(as_string=True).lower()).str
+            if self.cam.data_type_disabled.get() == 0
+            else None
+        )
+
     def make_data_key(self):
         source = "PV:{}".format(self.prefix)
         # This shape is expected to match arr.shape for the array.
@@ -118,11 +129,9 @@ class DetectorBase(ADBase):
             external="FILESTORE:",
         )
 
-        # If the data type isn't disabled, we assume it is accurate
-        if self.cam.data_type_disabled.get() == 0:
-            ret["dtype_numpy"] = np.dtype(
-                self.cam.data_type.get(as_string=True).lower()
-            ).str
+        dtype_numpy = self.dtype_numpy
+        if dtype_numpy is not None:
+            ret["dtype_numpy"] = dtype_numpy
 
         return ret
 
@@ -226,6 +235,10 @@ class PICamDetector(DetectorBase):
 class PilatusDetector(DetectorBase):
     _html_docs = ["pilatusDoc.html"]
     cam = C(cam.PilatusDetectorCam, "cam1:")
+
+    @property
+    def dtype_numpy(self) -> Optional[str]:
+        return np.dtype(np.int32).str
 
 
 class PixiradDetector(DetectorBase):
